243041177d4982f6cdb6ac22b4a56c1c7a2f3a9e
xieguigang <xie.guigang@gcmodeller.org>
2022-05-03 20:54:24 +0800

    convert raw data files

--- msconvert/Program.vb
+++ msconvert/Program.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.ComponentModel
+Imports System.Threading
 Imports Microsoft.VisualBasic.CommandLine
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 
@@ -7,20 +8,38 @@ Imports Microsoft.VisualBasic.CommandLine.Reflection
 ''' </summary>
 Module Program
 
+    Sub New()
+        Call Thread.Sleep(2000)
+    End Sub
+
     Public Function Main() As Integer
         Return GetType(Program).RunCLI(App.CommandLine)
     End Function
 
-    <ExportAPI("--mzPack")>
+    <ExportAPI("/mzPack")>
     <Description("Build mzPack cache")>
     <Argument("--raw", False, CLITypes.File, PipelineTypes.std_in, Description:="the file path of the mzML/mzXML/raw raw data file to create mzPack cache file.")>
     <Argument("--cache", False, CLITypes.File, PipelineTypes.std_out, Description:="the file path of the mzPack cache file.")>
-    <Usage("--mzPack --raw <filepath.mzXML> --cache <result.mzPack>")>
+    <Usage("/mzPack --raw <filepath.mzXML> --cache <result.mzPack>")>
     Public Function convertAnyRaw(args As CommandLine) As Integer
         Dim raw As String = args("--raw")
         Dim cache As String = args("--cache")
 
         Call ConvertToMzPack.CreateMzpack(raw, cache)
+
+        Return 0
+    End Function
+
+    <ExportAPI("/rowbinds")>
+    <Description("Combine row scans to mzPack")>
+    <Argument("--files", False, CLITypes.File, PipelineTypes.std_in, Description:="a temp file path that its content contains selected raw data file path for each row scans.")>
+    <Argument("--save", False, CLITypes.File, PipelineTypes.std_in, Description:="a file path for export mzPack data file.")>
+    <Usage("/rowbinds --files <list.txt> --save <MSI.mzPack>")>
+    Public Function MSIRowCombine(args As CommandLine) As Integer
+        Dim files As String() = args("--files").ReadAllLines
+        Dim save As String = args("--save")
+
+        Call MSImagingRowBinds.MSI_rowbind(files, save)
 
         Return 0
     End Function
