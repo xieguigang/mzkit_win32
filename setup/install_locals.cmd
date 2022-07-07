e2b5d063fcc96141da94bd15ecb1f4ed44514628
guigang xie <gg.xie@bionovogene.com>
2022-07-07 11:27:37 +0800

    Update install_locals.cmd

--- setup/install_locals.cmd
+++ setup/install_locals.cmd
@@ -3,6 +3,9 @@
 SET R_HOME=../bin/
 SET R_ENV="%R_HOME%/R#.exe"
 
+%R_ENV% --reset
+%R_ENV% --setup
+
 %R_ENV% --install.packages "./GCModeller.zip"
 %R_ENV% --install.packages "./ggplot.zip"
 %R_ENV% --install.packages "./markdown2pdf.zip"
