5af74c717d1a106aaf1724795366374a6d819183
guigang xie <gg.xie@bionovogene.com>
2022-07-14 15:00:19 +0800

    tweaks of the single ion ms-imaging blending

--- mzblender/MSImaging/SingleIonMSIBlender.vb
+++ mzblender/MSImaging/SingleIonMSIBlender.vb
@@ -10,13 +10,15 @@ Public Class SingleIonMSIBlender : Inherits Blender
 
     ReadOnly layer As SingleIonLayer
     ReadOnly params As MsImageProperty
+    ReadOnly intensity As Double()
 
     Public ReadOnly Property range As DoubleRange
 
     Sub New(layer As PixelData(), params As MsImageProperty)
         Me.layer = New SingleIonLayer With {.MSILayer = layer}
         Me.params = params
-        Me.range = layer.Select(Function(i) i.intensity).Range
+        Me.intensity = layer.Select(Function(i) i.intensity).ToArray
+        Me.range = intensity.Range
     End Sub
 
     Public Overrides Function Rendering(args As PlotProperty, target As Size) As Image
@@ -24,8 +26,11 @@ Public Class SingleIonMSIBlender : Inherits Blender
         Dim size As String = "2,2"
         Dim pixels As PixelData() = layer.MSILayer
         Dim pixelFilter As PixelData()
+        Dim cut As Double = New TrIQThreshold(params.TrIQ) With {
+            .levels = params.mapLevels
+        }.ThresholdValue(intensity)
 
-        pixelFilter = MsImaging.Drawer.ScalePixels(pixels, params.GetTolerance, cut:={0, 1})
+        pixelFilter = MsImaging.Drawer.ScalePixels(pixels, params.GetTolerance, cut:={0, cut})
         pixelFilter = MsImaging.Drawer.GetPixelsMatrix(pixelFilter)
 
         Dim drawer As New PixelRender(heatmapRender:=False)
