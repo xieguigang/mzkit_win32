487cb7557d8347eb0d38ce1f7ab58aaac03abcd6
guigang xie <gg.xie@bionovogene.com>
2022-07-28 12:21:50 +0800

    query search of the pubchem database

--- mzkit/forms/Inputs/InputPubChemProxy.vb
+++ mzkit/forms/Inputs/InputPubChemProxy.vb
@@ -1,4 +1,10 @@
-﻿Public Class InputPubChemProxy
+﻿Imports BioNovoGene.BioDeep.Chemistry.MetaLib.Models
+Imports BioNovoGene.BioDeep.Chemistry.NCBI.PubChem
+Imports Microsoft.VisualBasic.Linq
+
+Public Class InputPubChemProxy
+
+    ReadOnly cids As New Dictionary(Of String, MetaLib)
 
     ''' <summary>
     ''' do pubchem search
@@ -9,13 +15,28 @@
         If Strings.Trim(TextBox1.Text).StringEmpty Then
             Call MessageBox.Show("No query text input!", "PubChem Query", MessageBoxButtons.OK, MessageBoxIcon.Warning)
         Else
-            Call doSearch(TextBox1.Text)
+            Call cids.Clear()
+            Call doSearch(Strings.Trim(TextBox1.Text))
         End If
     End Sub
 
     Private Sub doSearch(text As String)
         ' text to cid
         ' then query by cid
+        Dim cids = Query.QueryCID(
+            name:=text,
+            cacheFolder:=$"{Globals.pubchemWebCache}/.cids/",
+            offlineMode:=False,
+            hitCache:=Nothing,
+            interval:=0
+        )
+        Dim api As WebQuery = $"{Globals.pubchemWebCache}/pugViews/".GetQueryHandler(Of WebQuery)(offline:=False)
 
+        For Each id As String In cids.SafeQuery
+            Dim compound = api.Query(Of PugViewRecord)(id)
+            Dim metadata = compound.GetMetaInfo
+
+            Call Me.cids.Add(id, metadata)
+        Next
     End Sub
 End Class
