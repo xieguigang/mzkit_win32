0c463c310cf4872c5bebd7c607b1a93458ad2ec8
guigang xie <gg.xie@bionovogene.com>
2022-08-08 13:11:06 +0800

    adjust default input size

--- ux/ControlLibrary/ToolStripSpringTextBox.vb
+++ ux/ControlLibrary/ToolStripSpringTextBox.vb
@@ -1,63 +1,66 @@
 ﻿#Region "Microsoft.VisualBasic::89afa5571800be291157f72b6b751c0e, mzkit\src\mzkit\ControlLibrary\ToolStripSpringTextBox.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 60
-    '    Code Lines: 29
-    ' Comment Lines: 19
-    '   Blank Lines: 12
-    '     File Size: 2.51 KB
+' Summaries:
 
 
-    ' Class ToolStripSpringTextBox
-    ' 
-    '     Function: GetPreferredSize
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 60
+'    Code Lines: 29
+' Comment Lines: 19
+'   Blank Lines: 12
+'     File Size: 2.51 KB
+
+
+' Class ToolStripSpringTextBox
+' 
+'     Function: GetPreferredSize
+' 
+' /********************************************************************************/
 
 #End Region
 
-Public Class ToolStripSpringTextBox
-    Inherits ToolStripTextBox
+Public Class ToolStripSpringTextBox : Inherits ToolStripTextBox
 
-    Public Overrides Function GetPreferredSize(
-        ByVal constrainingSize As Size) As Size
+    Protected Overrides ReadOnly Property DefaultSize As Size
+        Get
+            Return New Size(64, 22)
+        End Get
+    End Property
 
+    Public Overrides Function GetPreferredSize(constrainingSize As Size) As Size
         ' Use the default size if the text box is on the overflow menu
         ' or is on a vertical ToolStrip.
         If IsOnOverflow Or Owner.Orientation = Orientation.Vertical Then
