7afdbfc7ebd6c2a8ace4fd44427a41f9b293d6ef
guigang xie <gg.xie@bionovogene.com>
2022-08-09 10:44:43 +0800

    plot data msi

--- mzkit/forms/frmTweaks/frmMsImagingTweaks.vb
+++ mzkit/forms/frmTweaks/frmMsImagingTweaks.vb
@@ -465,7 +465,13 @@ UseCheckedList:
 
     Private Sub showPlot(data As Dictionary(Of String, NamedValue(Of Double())), type As String, mz As Double)
         Dim pack As String = encodeJSON(data)
-        Dim image = RscriptProgressTask.PlotStats(pack, type, title:=$"MZ: {mz.ToString("F4")}")
+        Dim image As Image
+
+        If AppendMSImagingToolStripMenuItem.Checked Then
+
+        Else
+            image = RscriptProgressTask.PlotStats(pack, type, title:=$"M/Z: {mz.ToString("F4")}")
+        End If
 
         If image Is Nothing Then
             MyApplication.host.showStatusMessage("Error while run ggplot...", My.Resources.StatusAnnotations_Warning_32xLG_color)
---
+++ rstudio/pipeline/ggplot/ionStatMSI.R
@@ -0,0 +1,91 @@
+imports "app" from "ServiceHub";
+
+require(MSImaging);
+require(mzkit);
+require(ggplot);
+
+options(memory.load = "max");
+
+const rawdata as string   = ?"--data"   || stop("no raw data provided!");
+const plot_type as string = ?"--plot"   || stop("should be one of the 'box', 'bar', 'violin'");
+const appPort as integer  = ?"--app"    || stop("A MSimaging services hub app handle must be provided!");
+const mz as string        = ?"--mzlist" || stop("target ions list must be provided!");
+const mzdiff as string    = ?"--mzdiff" || "da:0.1";
+const savefile as string  = ?"--save"   || stop("A file path to save plot image must be specificed!");
+const title as string     = ?"--title"  || NULL;
+const bg as string        = ?"--backcolor" || "black";
+const colorSet as string  = ?"--colors" || "viridis:turbo";
+const mzlist as double    = mz
+|> strsplit(",", fixed = TRUE)
+|> unlist()
+|> as.numeric()
+;
+const pixelsData = app::getMSIData(
+    MSI_service = appPort, 
+    mz          = mzlist, 
+    mzdiff      = mzdiff
+);
+const mz_tag as string = `m/z ${round(mzlist[1], 4)}`;
+const mzpack = pixelPack(pixelsData);
+
+let myeloma = rawdata
+|> readText()
+|> json_decode()
+;
+
+const intensity_data = list();
+const sampleinfo = list();
+const sample_color = lapply(myeloma, c -> c$color);
+
+for(name in names(myeloma)) {
+    let part = myeloma[[name]];
+    let color = part$color;
+    let group_id = `${name}_${1:length(part$data)}`;
+
+	part = part$data;
+	sampleinfo[[name]] = {
+        group: name,
+        id: group_id,
+        color: color
+    };
+    
+    for(i in 1:length(group_id)) {
+        intensity_data[[(group_id[i])]] = part[i];
+    }
+}
+
+print(`load ${length(pixelsData)} pixels data from given m/z:`);
+print(mzlist);
+
+const getGgplot = function() {
+	if (plot_type == "box") {
+		geom_boxplot(width = 0.65, alpha = 0.85, color = sample_color);
+	} else if (plot_type == "bar") {
+		geom_barplot(width = 0.65, alpha = 0.85, color = sample_color);
+	} else {
+		geom_violin(width = 0.65, alpha = 0.85, color = sample_color);
+	}
+}
+
+MSImaging::MSI_ionStatPlot(
+    mzpack         = mzpack, 
+    mz             = mzlist, 
+    met            = intensity_data, 
+    sampleinfo     = sampleinfo, 
+    savePng        = savefile, 
+    ionName        = title,
+    size           = [2400, 1000], 
+    colorMap       = NULL, 
+    MSI_colorset   = colorSet,
+    ggStatPlot     = getGgplot(), 
+    padding_top    = 150,
+    padding_right  = 200,
+    padding_bottom = 150,
+    padding_left   = 150,
+    interval       = 50,
+    combine_layout = [4, 5], 
+    jitter_size    = 8, 
+    TrIQ           = 0.65,
+    backcolor      = bg
+)
+;
