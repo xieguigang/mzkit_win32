d10cbdfc8b975ad08fa580a86e6092cdc4818206
guigang xie <xie.guigang@gcmodeller.org>
2022-08-20 01:33:06 +0800

    fix of the null reference error

--- msconvert/Commands/ConvertToMzPack.vb
+++ msconvert/Commands/ConvertToMzPack.vb
@@ -36,18 +36,27 @@ Module ConvertToMzPack
         End If
 
         If (Not mzpack.MS.IsNullOrEmpty) AndAlso (Not skipThumbnail) Then
-            println("Create snapshot...")
+            If Not println Is Nothing Then
+                println("Create snapshot...")
+            End If
+
             mzpack.Thumbnail = mzpack.DrawScatter
         End If
 
         ' mzpack = mzpack.MassCalibration(da:=0.1)
 
         Using file As Stream = cacheFile.Open(FileMode.OpenOrCreate, doClear:=True, [readOnly]:=False)
-            Call println("Write mzPack cache data...")
+            If Not println Is Nothing Then
+                Call println("Write mzPack cache data...")
+            End If
+
             Call mzpack.Write(file, version:=saveVer)
         End Using
 
         Call Thread.Sleep(sleepTime)
-        Call println("Job Done!")
+
+        If Not println Is Nothing Then
+            Call println("Job Done!")
+        End If
     End Sub
 End Module
