9a87c1eb175c44c50e8eea95848474c0663fe476
guigang xie <xie.guigang@gcmodeller.org>
2022-08-19 14:37:57 +0800

    improvements of the commandline

--- msconvert/Commands/ConvertToMzPack.vb
+++ msconvert/Commands/ConvertToMzPack.vb
@@ -13,30 +13,41 @@ Module ConvertToMzPack
     ''' </summary>
     ''' <param name="raw"></param>
     ''' <param name="cacheFile"></param>
-    Public Sub CreateMzpack(raw As String, cacheFile As String, saveVer As Integer)
+    Public Sub CreateMzpack(raw As String,
+                            cacheFile As String,
+                            saveVer As Integer,
+                            mute As Boolean,
+                            skipThumbnail As Boolean,
+                            Optional sleepTime As Double = 1500)
+
         Dim mzpack As mzPack
+        Dim println As Action(Of String) = Nothing
+
+        If Not mute Then
+            println = AddressOf RunSlavePipeline.SendMessage
+        End If
 
         If raw.ExtensionSuffix("raw") Then
             Using msraw As New MSFileReader(raw)
-                mzpack = msraw.LoadFromXRaw(AddressOf RunSlavePipeline.SendMessage)
+                mzpack = msraw.LoadFromXRaw(println)
             End Using
         Else
-            mzpack = Converter.LoadRawFileAuto(raw, "ppm:20", , AddressOf RunSlavePipeline.SendMessage)
+            mzpack = Converter.LoadRawFileAuto(raw, "ppm:20", , println)
         End If
 
-        If Not mzpack.MS.IsNullOrEmpty Then
-            RunSlavePipeline.SendMessage("Create snapshot...")
+        If (Not mzpack.MS.IsNullOrEmpty) AndAlso (Not skipThumbnail) Then
+            println("Create snapshot...")
             mzpack.Thumbnail = mzpack.DrawScatter
         End If
 
         ' mzpack = mzpack.MassCalibration(da:=0.1)
 
         Using file As Stream = cacheFile.Open(FileMode.OpenOrCreate, doClear:=True, [readOnly]:=False)
-            Call RunSlavePipeline.SendMessage("Write mzPack cache data...")
+            Call println("Write mzPack cache data...")
             Call mzpack.Write(file, version:=saveVer)
         End Using
 
-        Call Thread.Sleep(1500)
-        Call RunSlavePipeline.SendMessage("Job Done!")
+        Call Thread.Sleep(sleepTime)
+        Call println("Job Done!")
     End Sub
 End Module
