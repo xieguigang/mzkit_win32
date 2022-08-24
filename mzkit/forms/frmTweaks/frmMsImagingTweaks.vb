be93c634ec58fdc9ef7eb080b4b88127993f88c4
guigang xie <xie.guigang@gcmodeller.org>
2022-08-24 13:03:57 +0800

    plot tissue regions

--- mzkit/forms/frmTweaks/frmMsImagingTweaks.vb
+++ mzkit/forms/frmTweaks/frmMsImagingTweaks.vb
@@ -313,8 +313,21 @@ UseCheckedList:
 
         Using cdf As New netCDFReader(firstFile)
             If Not {"mz", "intensity", "x", "y"}.All(AddressOf cdf.dataVariableExists) Then
-                ' invalid format
-                Call MyApplication.host.showStatusMessage("Invalid cdf file format! [mz, intensity, x, y] data vector should exists inside this cdf file!", My.Resources.StatusAnnotations_Warning_32xLG_color)
+                If cdf.IsTissueMorphologyCDF Then
+                    Dim regions = cdf.ReadTissueMorphology.ToArray
+                    Dim dims As New Size With {
+                        .Width = regions.Select(Function(a) a.points.Select(Function(i) i.X)).IteratesALL.Max,
+                        .Height = regions.Select(Function(a) a.points.Select(Function(i) i.Y)).IteratesALL.Max
+                    }
+                    Dim Rplot As Image = LayerRender.Draw(regions, dims, alphaLevel:=1, dotSize:=3)
+
+                    Call MyApplication.host.mzkitTool.ShowPlotImage(Rplot)
+                    Call MyApplication.host.ShowMzkitToolkit()
+                Else
+                    ' invalid format
+                    Call MyApplication.host.showStatusMessage("Invalid cdf file format! [mz, intensity, x, y] data vector should exists inside this cdf file!", My.Resources.StatusAnnotations_Warning_32xLG_color)
+                End If
+
                 Return
             End If
 
