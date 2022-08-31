adf655f7697f4214b4e6f6b3f5740fba3d7140c6
xieguigang <xie.guigang@gcmodeller.org>
2022-08-31 21:02:02 +0800

    run scanning task

---
+++ rstudio/pipeline/HEScan.R
@@ -0,0 +1,20 @@
+require(mzkit);
+require(graphics2D);
+require(filter);
+require(JSON);
+
+imports "tissue" from "mzplot";
+
+const map as string      = ?"--bitmap"   || stop("no image source is provided!");
+const channels as string = ?"--channels" || stop("a set of color channels must be provided!");
+const savejson as string = ?"--save"     || NULL;
+const layers as string   = strsplit(channels, "[;,\/]") |> unlist();
+const grid = map
+|> readImage()
+|> tissue::scan_tissue(colors = layers)
+;
+
+grid
+|> json_encode()
+|> writeLines(con = savejson)
+;
