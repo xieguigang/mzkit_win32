46773ac013c618752587596baa263b16aba29e30
guigang xie <xie.guigang@gcmodeller.org>
2022-09-01 10:00:09 +0800

    add sub region

--- mzkit/pages/dockWindow/properties/SampleRegionMergeTool.Designer.vb
+++ mzkit/pages/dockWindow/properties/SampleRegionMergeTool.Designer.vb
@@ -22,18 +22,37 @@ Partial Class SampleRegionMergeTool
     '不要使用代码编辑器修改它。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.GroupBox2 = New System.Windows.Forms.GroupBox()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.CheckedListBox1 = New System.Windows.Forms.CheckedListBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.LinkLabel2 = New System.Windows.Forms.LinkLabel()
+        Me.LinkLabel3 = New System.Windows.Forms.LinkLabel()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ColorComboBox1 = New ControlLibrary.ColorComboBox()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.AddSubRegionToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.GroupBox1.SuspendLayout()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ContextMenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'GroupBox1
         '
+        Me.GroupBox1.Controls.Add(Me.ColorComboBox1)
+        Me.GroupBox1.Controls.Add(Me.Label3)
         Me.GroupBox1.Controls.Add(Me.PictureBox1)
         Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
         Me.GroupBox1.Name = "GroupBox1"
@@ -45,15 +64,23 @@ Partial Class SampleRegionMergeTool
         'PictureBox1
         '
         Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.ContextMenuStrip = Me.ContextMenuStrip1
         Me.PictureBox1.Cursor = System.Windows.Forms.Cursors.Cross
-        Me.PictureBox1.Location = New System.Drawing.Point(21, 18)
+        Me.PictureBox1.Location = New System.Drawing.Point(21, 46)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(592, 264)
+        Me.PictureBox1.Size = New System.Drawing.Size(592, 235)
         Me.PictureBox1.TabIndex = 0
         Me.PictureBox1.TabStop = False
         '
         'GroupBox2
         '
+        Me.GroupBox2.Controls.Add(Me.LinkLabel3)
+        Me.GroupBox2.Controls.Add(Me.LinkLabel2)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Controls.Add(Me.CheckedListBox1)
+        Me.GroupBox2.Controls.Add(Me.PictureBox2)
+        Me.GroupBox2.Controls.Add(Me.TextBox1)
+        Me.GroupBox2.Controls.Add(Me.LinkLabel1)
         Me.GroupBox2.Location = New System.Drawing.Point(12, 309)
         Me.GroupBox2.Name = "GroupBox2"
         Me.GroupBox2.Size = New System.Drawing.Size(634, 199)
@@ -81,17 +108,119 @@ Partial Class SampleRegionMergeTool
         '
         'Label1
         '
-        Me.Label1.Location = New System.Drawing.Point(12, 532)
+        Me.Label1.Location = New System.Drawing.Point(71, 526)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(434, 24)
+        Me.Label1.Size = New System.Drawing.Size(342, 24)
         Me.Label1.TabIndex = 4
         Me.Label1.Text = "Ready!"
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(19, 29)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(107, 12)
+        Me.LinkLabel1.TabIndex = 0
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "Create New Region"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(132, 23)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(179, 21)
+        Me.TextBox1.TabIndex = 1
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Location = New System.Drawing.Point(331, 20)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(70, 24)
+        Me.PictureBox2.TabIndex = 2
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Location = New System.Drawing.Point(12, 521)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(51, 24)
+        Me.PictureBox3.TabIndex = 3
+        Me.PictureBox3.TabStop = False
+        '
+        'CheckedListBox1
+        '
+        Me.CheckedListBox1.FormattingEnabled = True
+        Me.CheckedListBox1.Location = New System.Drawing.Point(21, 83)
+        Me.CheckedListBox1.Name = "CheckedListBox1"
+        Me.CheckedListBox1.Size = New System.Drawing.Size(290, 100)
+        Me.CheckedListBox1.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(19, 58)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(125, 12)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Selected SubRegions:"
+        '
+        'LinkLabel2
+        '
+        Me.LinkLabel2.AutoSize = True
+        Me.LinkLabel2.Location = New System.Drawing.Point(331, 83)
+        Me.LinkLabel2.Name = "LinkLabel2"
+        Me.LinkLabel2.Size = New System.Drawing.Size(35, 12)
+        Me.LinkLabel2.TabIndex = 5
+        Me.LinkLabel2.TabStop = True
+        Me.LinkLabel2.Text = "Merge"
+        '
+        'LinkLabel3
+        '
+        Me.LinkLabel3.AutoSize = True
+        Me.LinkLabel3.Location = New System.Drawing.Point(331, 106)
+        Me.LinkLabel3.Name = "LinkLabel3"
+        Me.LinkLabel3.Size = New System.Drawing.Size(35, 12)
+        Me.LinkLabel3.TabIndex = 6
+        Me.LinkLabel3.TabStop = True
+        Me.LinkLabel3.Text = "Clear"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(20, 22)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(119, 12)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Tissue Region Maps:"
+        '
+        'ColorComboBox1
+        '
+        Me.ColorComboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed
+        Me.ColorComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ColorComboBox1.FormattingEnabled = True
+        Me.ColorComboBox1.Location = New System.Drawing.Point(154, 16)
+        Me.ColorComboBox1.Name = "ColorComboBox1"
+        Me.ColorComboBox1.Size = New System.Drawing.Size(230, 22)
+        Me.ColorComboBox1.TabIndex = 2
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AddSubRegionToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(181, 48)
+        '
+        'AddSubRegionToolStripMenuItem
+        '
+        Me.AddSubRegionToolStripMenuItem.Name = "AddSubRegionToolStripMenuItem"
+        Me.AddSubRegionToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.AddSubRegionToolStripMenuItem.Text = "Add SubRegion"
         '
         'SampleRegionMergeTool
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(660, 553)
+        Me.Controls.Add(Me.PictureBox3)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
@@ -100,7 +229,13 @@ Partial Class SampleRegionMergeTool
         Me.Name = "SampleRegionMergeTool"
         Me.Text = "Merge Sample Regions"
         Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ContextMenuStrip1.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
@@ -111,4 +246,16 @@ Partial Class SampleRegionMergeTool
     Friend WithEvents Button2 As Button
     Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents Label1 As Label
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents CheckedListBox1 As CheckedListBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents LinkLabel3 As LinkLabel
+    Friend WithEvents LinkLabel2 As LinkLabel
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ColorComboBox1 As ControlLibrary.ColorComboBox
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents AddSubRegionToolStripMenuItem As ToolStripMenuItem
 End Class
--- mzkit/pages/dockWindow/properties/SampleRegionMergeTool.vb
+++ mzkit/pages/dockWindow/properties/SampleRegionMergeTool.vb
@@ -8,16 +8,20 @@ Public Class SampleRegionMergeTool
     Dim Rplot As Image
 
     Public Sub LoadRegions(regions As TissueRegion())
+        For Each r As TissueRegion In regions
+            rawRegions(r.label) = r
+            newRegions(r.label) = r
+        Next
+
+        Call Rendering(regions)
+    End Sub
+
+    Private Sub Rendering(regions As TissueRegion())
         Dim dims As New Size With {
             .Width = regions.Select(Function(a) a.points.Select(Function(i) i.X)).IteratesALL.Max,
             .Height = regions.Select(Function(a) a.points.Select(Function(i) i.Y)).IteratesALL.Max
         }
         Dim Rplot As Image = LayerRender.Draw(regions, dims, alphaLevel:=1, dotSize:=1)
-
-        For Each r As TissueRegion In regions
-            rawRegions(r.label) = r
-            newRegions(r.label) = r
-        Next
 
         Me.Rplot = Rplot
         Me.PictureBox1.BackgroundImage = Rplot
@@ -47,7 +51,7 @@ Public Class SampleRegionMergeTool
     End Sub
 
     Private Sub SampleRegionMergeTool_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        CheckedListBox1.Items.Clear()
     End Sub
 
     Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
@@ -67,10 +71,103 @@ Public Class SampleRegionMergeTool
 
         If region Is Nothing Then
             label = "No tissue region."
+            PictureBox3.BackColor = Color.Black
         Else
             label = $"{region.ToString}"
+            PictureBox3.BackColor = region.color
         End If
 
         Label1.Text = $"[{px},{py}] {label}"
+    End Sub
+
+    ''' <summary>
+    ''' add new region
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        If CheckedListBox1.Items.Count > 0 Then
+            If MessageBox.Show("There are unmerge region list, still going to create a new region?", "Create New Region", MessageBoxButtons.OKCancel, MessageBoxIcon.Information) = DialogResult.Cancel Then
+                Return
+            End If
+        End If
+
+        CheckedListBox1.Items.Clear()
+        TextBox1.Clear()
+        PictureBox2.BackColor = Color.Black
+    End Sub
+
+    ''' <summary>
+    ''' clear
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub LinkLabel3_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel3.LinkClicked
+        CheckedListBox1.Items.Clear()
+    End Sub
+
+    ''' <summary>
+    ''' merge
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub LinkLabel2_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel2.LinkClicked
+        Dim color As Color = PictureBox2.BackColor
+        Dim label As String = TextBox1.Text
+        Dim subregions As New List(Of TissueRegion)
+
+        If label.StringEmpty Then
+            MessageBox.Show("region label can not be empty!", "Merge Region", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        ElseIf CheckedListBox1.CheckedItems.Count = 0 Then
+            MessageBox.Show("No sub-region is selected!", "Merge Region", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        For i As Integer = 0 To CheckedListBox1.CheckedItems.Count - 1
+            subregions.Add(CheckedListBox1.CheckedItems.Item(i))
+            newRegions.Remove(subregions.Last.label)
+        Next
+
+        newRegions(label) = New TissueRegion With {
+            .label = label,
+            .color = color,
+            .points = subregions _
+                .Select(Function(a) a.points) _
+                .IteratesALL _
+                .ToArray
+        }
+
+        Call Rendering(newRegions.Values.ToArray)
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+        Using colorDiag As New ColorDialog
+            If colorDiag.ShowDialog = DialogResult.OK Then
+                PictureBox2.BackColor = colorDiag.Color
+            End If
+        End Using
+    End Sub
+
+    Private Sub AddSubRegionToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AddSubRegionToolStripMenuItem.Click
+        Dim p As Point = PointToClient(MousePosition)
+        Dim px As Integer
+        Dim py As Integer
+
+        Call getPoint(p, Rplot.Size, px, py)
+
+        Dim region As TissueRegion = newRegions.Values _
+            .Where(Function(r) r.IsInside(px, py)) _
+            .FirstOrDefault
+
+        If region Is Nothing Then
+            Label1.Text = $"No tissue subregion at here!"
+            Return
+        ElseIf CheckedListBox1.Items.IndexOf(region) = -1 Then
+            CheckedListBox1.Items.Add(region)
+            Label1.Text = $"Add subregion {region}!"
+        Else
+            Label1.Text = $"Subregion {region} is already been added."
+        End If
     End Sub
 End Class
