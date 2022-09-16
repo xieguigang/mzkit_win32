a68e73ba917623b72459591647bdce86073de788
xieguigang <xie.guigang@gcmodeller.org>
2022-09-17 00:37:49 +0800

    Create ChromatogramBlender.vb

---
+++ mzblender/ChromatogramPlot/ChromatogramBlender.vb
@@ -0,0 +1,8 @@
+﻿Imports Task
+
+Public Class ChromatogramBlender : Inherits Blender
+
+    Public Overrides Function Rendering(args As PlotProperty, target As Size) As Image
+
+    End Function
+End Class
