5ba0f4e71d64e0c9f049776afcfa7943916ab9ab
xieguigang <xie.guigang@gcmodeller.org>
2022-09-20 01:15:32 +0800

    Update debug.cmd

--- rstudio/debug.cmd
+++ rstudio/debug.cmd
@@ -4,4 +4,4 @@ SET debug_port=33361
 SET localhost="Rstudio/Pipeline/ServiceHub/MSI-host.R"
 
 START mzkit_win32 --debug --port=%debug_port%
-START Rscript %localhost% --debug=%debug_port%
+START Rscript %localhost% --debug=%debug_port% --SetDllDirectory \mzkit\dist\bin\Rstudio\host
