bac75c8e938d794d67fad0a89d4833df7e7a8e4a
guigang xie <xie.guigang@gcmodeller.org>
2022-10-13 16:36:27 +0800

    Update plot_tissue.R

--- rstudio/pipeline/MSImaging/plot_tissue.R
+++ rstudio/pipeline/MSImaging/plot_tissue.R
@@ -9,7 +9,9 @@ const data = tissue_file
 |> loadTissue()
 |> as.data.frame()
 ;
-		
+	
+data[, "label"] = basename(data[, "label"]);
+	
 print("view tissue map matrix data:");
 print(data, max.print = 13);
 		
@@ -20,13 +22,13 @@ const colorMaps = lapply(1:length(labels), i -> colors[i], names = labels);
 print("get unique color maps for each tissue region:");
 str(colorMaps);
 		
-bitmap(file = save_png, size = [7600, 2100]) {
+bitmap(file = save_png, size = [8600, 2100]) {
 	# open graphics device and then do plot
-	ggplot(data, aes(x = "x", y = "y", color = "label"), padding = "padding:300px 450px 200px 250px;")
+	ggplot(data, aes(x = "x", y = "y", color = "label"), padding = "padding:300px 1600px 200px 250px;")
 	+ geom_point(aes(color = "label"), shape = "rectangle", size = 30)
 	+ scale_colour_manual(values = colorMaps)
 	+ labs(x = "X", y = "Y")
-	+ ggtitle(`Tissue Morphology - ${basename(file)}`)
+	+ ggtitle(`Tissue Morphology - ${basename(tissue_file)}`)
 	+ scale_x_continuous(labels = "F0")
 	+ scale_y_continuous(labels = "F0")
 	+ scale_y_reverse()
