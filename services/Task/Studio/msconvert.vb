3ce15dc3733ce80c1eea4593febf59d74b703b81
guigang xie <xie.guigang@gcmodeller.org>
2022-11-07 16:00:14 +0800

    update cli wrapper

--- services/Task/Studio/msconvert.vb
+++ services/Task/Studio/msconvert.vb
@@ -13,13 +13,13 @@ Imports Microsoft.VisualBasic.ApplicationServices
 '  // 
 '  // VERSION:   1.0.0.0
 '  // ASSEMBLY:  msconvert, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-'  // COPYRIGHT: Copyright (c)  2022
+'  // COPYRIGHT: Copyright (c) gg.xie@bionovogene.com 2022
 '  // GUID:      9fee3ffe-05c8-4aa5-bbbb-c5ff3a18b1d6
 '  // BUILT:     1/1/2000 12:00:00 AM
 '  // 
 ' 
 ' 
-'  < msconvert.Program 
+'  < msconvert.Program >
 ' 
 ' 
 ' SYNOPSIS
@@ -27,9 +27,10 @@ Imports Microsoft.VisualBasic.ApplicationServices
 ' 
 ' All of the command that available in this program has been list below:
 ' 
+'  /cdf_to_mzpack:        Convert GCMS un-targetted CDF raw data file to mzPack
 '  /imports-SCiLSLab:     
-'  /mzPack:               Build mzPack cach
-'  /rowbinds:             Combine row scans to mzPac
+'  /mzPack:               Build mzPack cache
+'  /rowbinds:             Combine row scans to mzPack
 ' 
 ' 
 ' ----------------------------------------------------------------------------------------------------
@@ -50,7 +51,7 @@ Public Class msconvert : Inherits InteropService
     Public Const App$ = "msconvert.exe"
 
     Sub New(App$)
-        MyBase._executableAssembly = App$
+        Call MyBase.New(app:=App$)
     End Sub
         
 ''' <summary>
@@ -65,11 +66,62 @@ Public Class msconvert : Inherits InteropService
 
 ''' <summary>
 ''' ```bash
+''' /cdf_to_mzpack --raw &lt;filepath.cdf&gt; [--cache &lt;result.mzPack&gt; /ver 2 /mute /no-thumbnail]
+''' ```
+''' Convert GCMS un-targetted CDF raw data file to mzPack.
+''' </summary>
+'''
+
+Public Function convertGCMSCDF(raw As String, 
+                                  Optional cache As String = "", 
+                                  Optional ver As String = "", 
+                                  Optional mute As Boolean = False, 
+                                  Optional no_thumbnail As Boolean = False) As Integer
+Dim cli = GetconvertGCMSCDFCommandLine(raw:=raw, 
+                                  cache:=cache, 
+                                  ver:=ver, 
+                                  mute:=mute, 
+                                  no_thumbnail:=no_thumbnail, internal_pipelineMode:=True)
+    Dim proc As IIORedirectAbstract = RunDotNetApp(cli)
+    Return proc.Run()
+End Function
+Public Function GetconvertGCMSCDFCommandLine(raw As String, 
+                                  Optional cache As String = "", 
+                                  Optional ver As String = "", 
+                                  Optional mute As Boolean = False, 
+                                  Optional no_thumbnail As Boolean = False, Optional internal_pipelineMode As Boolean = True) As String
+    Dim CLI As New StringBuilder("/cdf_to_mzpack")
+    Call CLI.Append(" ")
+    Call CLI.Append("--raw " & """" & raw & """ ")
+    If Not cache.StringEmpty Then
+            Call CLI.Append("--cache " & """" & cache & """ ")
+    End If
+    If Not ver.StringEmpty Then
+            Call CLI.Append("/ver " & """" & ver & """ ")
+    End If
+    If mute Then
+        Call CLI.Append("/mute ")
+    End If
+    If no_thumbnail Then
+        Call CLI.Append("/no-thumbnail ")
+    End If
+     Call CLI.Append($"/@set --internal_pipeline={internal_pipelineMode.ToString.ToUpper()} ")
+
+
+Return CLI.ToString()
+End Function
+
+''' <summary>
+''' ```bash
 ''' /imports-SCiLSLab --files &lt;spot_files.txt&gt; --save &lt;MSI.mzPack&gt;
 ''' ```
 ''' </summary>
 '''
-
+''' <param name="files"> a list of the csv data files, the spot index and
+'''               the mass data should be paired in this data file list. each line 
+'''               of this text file is a tuple of the spot index and the ms data, 
+'''               and the tuple data should be used the &lt;TAB&gt; as delimiter.
+''' </param>
 Public Function ImportsSCiLSLab(files As String, save As String) As Integer
 Dim cli = GetImportsSCiLSLabCommandLine(files:=files, save:=save, internal_pipelineMode:=True)
     Dim proc As IIORedirectAbstract = RunDotNetApp(cli)
@@ -88,7 +140,7 @@ End Function
 
 ''' <summary>
 ''' ```bash
-''' /mzPack --raw &lt;filepath.mzXML&gt; --cache &lt;result.mzPack&gt;
+''' /mzPack --raw &lt;filepath.mzXML&gt; [--cache &lt;result.mzPack&gt; /ver 2 /mute /no-thumbnail]
 ''' ```
 ''' Build mzPack cache
 ''' </summary>
@@ -97,16 +149,41 @@ End Function
 ''' </param>
 ''' <param name="cache"> the file path of the mzPack cache file.
 ''' </param>
-Public Function convertAnyRaw(raw As String, cache As String) As Integer
-Dim cli = GetconvertAnyRawCommandLine(raw:=raw, cache:=cache, internal_pipelineMode:=True)
+''' <param name="ver"> the file format version of the generated mzpack data file
+''' </param>
+Public Function convertAnyRaw(raw As String, 
+                                 Optional cache As String = "", 
+                                 Optional ver As String = "", 
+                                 Optional mute As Boolean = False, 
+                                 Optional no_thumbnail As Boolean = False) As Integer
+Dim cli = GetconvertAnyRawCommandLine(raw:=raw, 
+                                 cache:=cache, 
+                                 ver:=ver, 
+                                 mute:=mute, 
+                                 no_thumbnail:=no_thumbnail, internal_pipelineMode:=True)
     Dim proc As IIORedirectAbstract = RunDotNetApp(cli)
     Return proc.Run()
 End Function
-Public Function GetconvertAnyRawCommandLine(raw As String, cache As String, Optional internal_pipelineMode As Boolean = True) As String
+Public Function GetconvertAnyRawCommandLine(raw As String, 
+                                 Optional cache As String = "", 
+                                 Optional ver As String = "", 
+                                 Optional mute As Boolean = False, 
+                                 Optional no_thumbnail As Boolean = False, Optional internal_pipelineMode As Boolean = True) As String
     Dim CLI As New StringBuilder("/mzPack")
     Call CLI.Append(" ")
     Call CLI.Append("--raw " & """" & raw & """ ")
-    Call CLI.Append("--cache " & """" & cache & """ ")
+    If Not cache.StringEmpty Then
+            Call CLI.Append("--cache " & """" & cache & """ ")
+    End If
+    If Not ver.StringEmpty Then
+            Call CLI.Append("/ver " & """" & ver & """ ")
+    End If
+    If mute Then
+        Call CLI.Append("/mute ")
+    End If
+    If no_thumbnail Then
+        Call CLI.Append("/no-thumbnail ")
+    End If
      Call CLI.Append($"/@set --internal_pipeline={internal_pipelineMode.ToString.ToUpper()} ")
 
 
@@ -141,6 +218,4 @@ Return CLI.ToString()
 End Function
 End Class
 End Namespace
-
-
 
