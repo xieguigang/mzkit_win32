d92e50ef1e9d9cf4a7d1e3fac80fcecd24079e40
xieguigang <xie.guigang@gcmodeller.org>
2022-11-14 21:25:05 +0800

    Update frmSMILESViewer.Designer.vb

--- mzkit/pages/dockWindow/documents/frmSMILESViewer.Designer.vb
+++ mzkit/pages/dockWindow/documents/frmSMILESViewer.Designer.vb
@@ -81,7 +81,6 @@ Partial Class frmSMILESViewer
         Me.Button1 = New System.Windows.Forms.Button()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.Canvas1 = New Microsoft.VisualBasic.Data.visualize.Network.Canvas.Canvas()
         Me.TabControl1 = New System.Windows.Forms.TabControl()
         Me.TabPage1 = New System.Windows.Forms.TabPage()
         Me.TabPage2 = New System.Windows.Forms.TabPage()
@@ -98,7 +97,7 @@ Partial Class frmSMILESViewer
         'Button1
         '
         Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
-        Me.Button1.Location = New System.Drawing.Point(497, 573)
+        Me.Button1.Location = New System.Drawing.Point(497, 396)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 0
@@ -108,9 +107,9 @@ Partial Class frmSMILESViewer
         'TextBox1
         '
         Me.TextBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
-        Me.TextBox1.Location = New System.Drawing.Point(18, 575)
+        Me.TextBox1.Location = New System.Drawing.Point(18, 398)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(473, 21)
+        Me.TextBox1.Size = New System.Drawing.Size(473, 20)
         Me.TextBox1.TabIndex = 1
         Me.TextBox1.Text = "CC(=O)OC1=CC=CC=C1C(=O)O"
         '
@@ -118,28 +117,11 @@ Partial Class frmSMILESViewer
         '
         Me.Label1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(15, 550)
+        Me.Label1.Location = New System.Drawing.Point(15, 373)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(83, 12)
+        Me.Label1.Size = New System.Drawing.Size(76, 13)
         Me.Label1.TabIndex = 2
         Me.Label1.Text = "Input SMILES:"
-        '
-        'Canvas1
-        '
-        Me.Canvas1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.Canvas1.AutoRotate = True
-        Me.Canvas1.BackColor = System.Drawing.Color.FromArgb(CType(CType(219, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(255, Byte), Integer))
-        Me.Canvas1.DynamicsRadius = False
-        Me.Canvas1.Font = New System.Drawing.Font("Microsoft YaHei UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Canvas1.Location = New System.Drawing.Point(6, 7)
-        Me.Canvas1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
-        Me.Canvas1.Name = "Canvas1"
-        Me.Canvas1.ShowLabel = False
-        Me.Canvas1.Size = New System.Drawing.Size(1066, 526)
-        Me.Canvas1.TabIndex = 3
-        Me.Canvas1.ViewDistance = 0R
         '
         'TabControl1
         '
@@ -149,19 +131,18 @@ Partial Class frmSMILESViewer
         Me.TabControl1.Location = New System.Drawing.Point(0, 0)
         Me.TabControl1.Name = "TabControl1"
         Me.TabControl1.SelectedIndex = 0
-        Me.TabControl1.Size = New System.Drawing.Size(1024, 750)
+        Me.TabControl1.Size = New System.Drawing.Size(1024, 573)
         Me.TabControl1.TabIndex = 4
         '
         'TabPage1
         '
-        Me.TabPage1.Controls.Add(Me.Canvas1)
         Me.TabPage1.Controls.Add(Me.Label1)
         Me.TabPage1.Controls.Add(Me.TextBox1)
         Me.TabPage1.Controls.Add(Me.Button1)
         Me.TabPage1.Location = New System.Drawing.Point(4, 22)
         Me.TabPage1.Name = "TabPage1"
         Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage1.Size = New System.Drawing.Size(1016, 724)
+        Me.TabPage1.Size = New System.Drawing.Size(1016, 547)
         Me.TabPage1.TabIndex = 0
         Me.TabPage1.Text = "SMILES Drawer"
         Me.TabPage1.UseVisualStyleBackColor = True
@@ -219,15 +200,15 @@ Partial Class frmSMILESViewer
         Me.Label2.AutoSize = True
         Me.Label2.Location = New System.Drawing.Point(19, 19)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(95, 12)
+        Me.Label2.Size = New System.Drawing.Size(81, 13)
         Me.Label2.TabIndex = 0
         Me.Label2.Text = "Input KCF data:"
         '
         'frmSMILESViewer
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1024, 750)
+        Me.ClientSize = New System.Drawing.Size(1024, 573)
         Me.Controls.Add(Me.TabControl1)
         Me.DoubleBuffered = True
         Me.Name = "frmSMILESViewer"
@@ -244,7 +225,7 @@ Partial Class frmSMILESViewer
     Friend WithEvents Button1 As Button
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Label1 As Label
-    Friend WithEvents Canvas1 As Microsoft.VisualBasic.Data.visualize.Network.Canvas.Canvas
+
     Friend WithEvents TabControl1 As TabControl
     Friend WithEvents TabPage1 As TabPage
     Friend WithEvents TabPage2 As TabPage
