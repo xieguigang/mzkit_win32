eaf7efa76c99ffe1713d768d505e368dcce5dbe4
guigang xie <xie.guigang@gcmodeller.org>
2022-11-18 14:38:12 +0800

    fix of the namespace

--- ux/ControlLibrary/WinForms/CommandLinkButton.vb
+++ ux/ControlLibrary/WinForms/CommandLinkButton.vb
@@ -1,19 +1,17 @@
 ﻿Imports System.Windows.Forms
 
-Namespace System.Windows.Forms
-    Partial Public Class CommandLinkButton
-        Inherits Button
-        Private Const BS_COMMANDLINK As Integer = &HE
-        Protected Overrides ReadOnly Property CreateParams As CreateParams
-            Get
-                Dim cParams = MyBase.CreateParams
-                cParams.Style = cParams.Style Or BS_COMMANDLINK
-                Return cParams
-            End Get
-        End Property
-        Public Sub New()
-            InitializeComponent()
-            FlatStyle = FlatStyle.System
-        End Sub
-    End Class
-End Namespace
+Partial Public Class CommandLinkButton
+    Inherits Button
+    Private Const BS_COMMANDLINK As Integer = &HE
+    Protected Overrides ReadOnly Property CreateParams As CreateParams
+        Get
+            Dim cParams = MyBase.CreateParams
+            cParams.Style = cParams.Style Or BS_COMMANDLINK
+            Return cParams
+        End Get
+    End Property
+    Public Sub New()
+        InitializeComponent()
+        FlatStyle = FlatStyle.System
+    End Sub
+End Class
