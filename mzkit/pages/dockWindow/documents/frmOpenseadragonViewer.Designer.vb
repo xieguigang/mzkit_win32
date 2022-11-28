d2d529b9497fd76345df660b4b475f2e1d52e3d8
xieguigang <xie.guigang@gcmodeller.org>
2022-11-28 22:12:50 +0800

    create local web server for dzi viewer

---
+++ mzkit/pages/dockWindow/documents/frmOpenseadragonViewer.Designer.vb
@@ -0,0 +1,55 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmOpenseadragonViewer
+    Inherits DocumentWindow
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.WebView21 = New Microsoft.Web.WebView2.WinForms.WebView2()
+        CType(Me.WebView21, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'WebView21
+        '
+        Me.WebView21.AllowExternalDrop = True
+        Me.WebView21.CreationProperties = Nothing
+        Me.WebView21.DefaultBackgroundColor = System.Drawing.Color.White
+        Me.WebView21.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.WebView21.Location = New System.Drawing.Point(0, 0)
+        Me.WebView21.Name = "WebView21"
+        Me.WebView21.Size = New System.Drawing.Size(800, 450)
+        Me.WebView21.TabIndex = 0
+        Me.WebView21.ZoomFactor = 1.0R
+        '
+        'frmOpenseadragonViewer
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.WebView21)
+        Me.Name = "frmOpenseadragonViewer"
+        Me.Text = "Form1"
+        CType(Me.WebView21, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents WebView21 As Microsoft.Web.WebView2.WinForms.WebView2
+End Class
---
+++ mzkit/pages/dockWindow/documents/frmOpenseadragonViewer.vb
@@ -0,0 +1,64 @@
+﻿Imports System.ComponentModel
+Imports BioNovoGene.mzkit_win32.My
+Imports Microsoft.VisualBasic.ApplicationServices
+Imports Microsoft.Web.WebView2.Core
+Imports Mzkit_win32.MSImagingViewerV2.DeepZoomBuilder
+
+Public Class frmOpenseadragonViewer
+
+    Dim dzi As String
+    Dim localfs As Process
+    Dim webPort As Integer
+    Dim sourcefile As String
+
+    Public ReadOnly Property sourceURL As String
+        Get
+            Return $"http://127.0.0.1:{Globals.WebPort}/openseadragon.html"
+        End Get
+    End Property
+
+    Public Sub LoadSlide(tiff As String)
+        If tiff.ExtensionSuffix("xml", "dzi") Then
+            dzi = tiff
+        Else
+            dzi = TempFileSystem.GetAppSysTempFile(".dzi", sessionID:=App.PID.ToHexString.MD5.Substring(2, 6) & "-" & tiff.BaseName, prefix:="deep_zoom_")
+            Call New DeepZoomCreator().CreateSingleComposition(tiff, dzi, ImageType.Jpeg)
+        End If
+
+        Call startHttp()
+        Call frmHtmlViewer.Init(Me.WebView21)
+    End Sub
+
+    Private Sub startHttp()
+        webPort = Net.Tcp.GetFirstAvailablePort(6000)
+        localfs = New Process With {
+            .StartInfo = New ProcessStartInfo With {
+                .FileName = $"{App.HOME}/Rstudio/bin/Rserve.exe",
+                .Arguments = $"--listen /wwwroot ""{dzi.ParentPath}"" /port {webPort}",
+                .CreateNoWindow = True,
+                .WindowStyle = ProcessWindowStyle.Hidden,
+                .UseShellExecute = True
+            }
+        }
+
+        Call localfs.Start()
+    End Sub
+
+    Private Sub frmOpenseadragonViewer_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        Call localfs.Kill()
+    End Sub
+
+    Private Sub WebView21_CoreWebView2InitializationCompleted(sender As Object, e As CoreWebView2InitializationCompletedEventArgs) Handles WebView21.CoreWebView2InitializationCompleted
+        Call WebView21.CoreWebView2.Navigate(sourceURL)
+        Call DeveloperOptions(enable:=True)
+    End Sub
+
+    Public Sub DeveloperOptions(enable As Boolean)
+        WebView21.CoreWebView2.Settings.AreDevToolsEnabled = enable
+        WebView21.CoreWebView2.Settings.AreBrowserAcceleratorKeysEnabled = enable
+        WebView21.CoreWebView2.Settings.AreDefaultContextMenusEnabled = enable
+        If enable Then
+            Call MyApplication.host.showStatusMessage($"[{TabText}] WebView2 developer tools has been enable!")
+        End If
+    End Sub
+End Class
