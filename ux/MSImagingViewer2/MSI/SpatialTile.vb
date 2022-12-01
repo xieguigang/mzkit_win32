8c4076aef16fa3530c8e95fbe7f33d5760650bb8
guigang xie <xie.guigang@gcmodeller.org>
2022-12-01 14:28:45 +0800

    Update SpatialTile.vb

--- ux/MSImagingViewer2/MSI/SpatialTile.vb
+++ ux/MSImagingViewer2/MSI/SpatialTile.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Drawing
+Imports Microsoft.VisualBasic.ComponentModel.Ranges.Model
 Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Imaging.Drawing2D.Colors
 Imports Microsoft.VisualBasic.Imaging.Drawing2D.HeatMap
@@ -14,7 +15,19 @@ Public Class SpatialTile
     End Sub
 
     Public Sub Plot()
+        Dim colors As SolidBrush() = Designer _
+            .GetColors(Me.colors.Description, 24) _
+            .Select(Function(c) New SolidBrush(c)) _
+            .ToArray
+        Dim range As New DoubleRange(spatialMatrix.Select(Function(i) i.Scale))
+        Dim index As New DoubleRange(0, colors.Length)
+
         Using g = Me.Size.CreateGDIDevice(filled:=Color.Transparent)
+            For Each spot As PixelData In spatialMatrix
+                Call g.DrawCircle(New Point(spot.X, spot.Y), radius, colors(CInt(range.ScaleMapping(spot.Scale, index))))
+            Next
+
+            Call g.Flush()
 
         End Using
     End Sub
