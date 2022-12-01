9da9812469e7255d6d657f523ce20021d5c27564
guigang xie <xie.guigang@gcmodeller.org>
2022-12-01 11:40:33 +0800

    try to make mapping between two spatial data

---
+++ ux/MSImagingViewer2/MSI/SpatialTile.Designer.vb
@@ -0,0 +1,59 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class SpatialTile
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl 重写释放以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.AdjustRadiusToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AdjustRadiusToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(147, 26)
+        '
+        'AdjustRadiusToolStripMenuItem
+        '
+        Me.AdjustRadiusToolStripMenuItem.Name = "AdjustRadiusToolStripMenuItem"
+        Me.AdjustRadiusToolStripMenuItem.Size = New System.Drawing.Size(146, 22)
+        Me.AdjustRadiusToolStripMenuItem.Text = "Adjust Radius"
+        '
+        'SpatialTile
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.DoubleBuffered = True
+        Me.Name = "SpatialTile"
+        Me.Size = New System.Drawing.Size(309, 304)
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents AdjustRadiusToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ ux/MSImagingViewer2/MSI/SpatialTile.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Drawing
+Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Imaging.Drawing2D.Colors
+Imports Microsoft.VisualBasic.Imaging.Drawing2D.HeatMap
+
+Public Class SpatialTile
+
+    Dim spatialMatrix As PixelData()
+    Dim colors As ScalerPalette
+    Dim radius As Integer = 10
+
+    Private Sub AdjustRadius() Handles AdjustRadiusToolStripMenuItem.Click
+
+    End Sub
+
+    Public Sub Plot()
+        Using g = Me.Size.CreateGDIDevice(filled:=Color.Transparent)
+
+        End Using
+    End Sub
+End Class
