1bfb6dbb5e554134241607f5114e56bc69730390
guigang xie <xie.guigang@gcmodeller.org>
2022-12-01 17:45:42 +0800

    config spatial plot

---
+++ ux/MSImagingViewer2/MSI/InputSpotRadius.Designer.vb
@@ -0,0 +1,102 @@
+﻿Imports CommonDialogs
+
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class InputSpotRadius
+    Inherits InputDialog
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(223, 61)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Set Radius"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(153, 90)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "OK"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(72, 90)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Cancel"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(17, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(77, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Spot Radius:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(103, 23)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 21)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "30"
+        '
+        'InputSpotRadius
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(246, 127)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "InputSpotRadius"
+        Me.Text = "Set Spot Radius"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+End Class
--- ux/MSImagingViewer2/MSI/SpatialTile.vb
+++ ux/MSImagingViewer2/MSI/SpatialTile.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Drawing
+Imports CommonDialogs
 Imports Microsoft.VisualBasic.ComponentModel.Ranges.Model
 Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Imaging.Drawing2D.Colors
@@ -9,9 +10,22 @@ Public Class SpatialTile
     Dim spatialMatrix As PixelData()
     Dim colors As ScalerPalette
     Dim radius As Integer = 10
+    Dim dimensions As Size
+
+    Public Sub ShowMatrix(dims As Size, matrix As IEnumerable(Of PixelData))
+        Me.dimensions = dims
+        Me.spatialMatrix = matrix.ToArray
+    End Sub
 
     Private Sub AdjustRadius() Handles AdjustRadiusToolStripMenuItem.Click
+        Dim input As New InputSpotRadius
+        input.TextBox1.Text = radius
 
+        InputDialog.Input(
+            Sub(config)
+                radius = Val(config.TextBox1.Text)
+                Plot()
+            End Sub,, config:=input)
     End Sub
 
     Public Sub Plot()
@@ -22,13 +36,14 @@ Public Class SpatialTile
         Dim range As New DoubleRange(spatialMatrix.Select(Function(i) i.Scale))
         Dim index As New DoubleRange(0, colors.Length)
 
-        Using g = Me.Size.CreateGDIDevice(filled:=Color.Transparent)
+        Using g As Graphics2D = dimensions.CreateGDIDevice(filled:=Color.Transparent)
             For Each spot As PixelData In spatialMatrix
                 Call g.DrawCircle(New Point(spot.X, spot.Y), radius, colors(CInt(range.ScaleMapping(spot.Scale, index))))
             Next
 
             Call g.Flush()
 
+            Me.BackgroundImage = g.ImageResource
         End Using
     End Sub
 End Class
