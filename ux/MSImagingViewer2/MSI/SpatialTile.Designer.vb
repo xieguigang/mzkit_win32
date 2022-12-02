81970cdc9f62c70fe7724309a4be83416b5bfa2e
guigang xie <xie.guigang@gcmodeller.org>
2022-12-02 15:54:06 +0800

    fix of the resource reference error

--- ux/MSImagingViewer2/MSI/SpatialTile.Designer.vb
+++ ux/MSImagingViewer2/MSI/SpatialTile.Designer.vb
@@ -32,7 +32,7 @@ Partial Class SpatialTile
         Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
         Me.Label1 = New System.Windows.Forms.Label()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox2 = New CustomDrawPictureBox()
+        Me.PictureBox2 = New Mzkit_win32.MSImagingViewerV2.CustomDrawPictureBox()
         Me.ContextMenuStrip1.SuspendLayout()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -85,7 +85,7 @@ Partial Class SpatialTile
         '
         'PictureBox2
         '
-        ' resources.ApplyResources(Me.PictureBox2, "PictureBox2")
+        resources.ApplyResources(Me.PictureBox2, "PictureBox2")
         Me.PictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.PictureBox2.ContextMenuStrip = Me.ContextMenuStrip1
         Me.PictureBox2.Name = "PictureBox2"
