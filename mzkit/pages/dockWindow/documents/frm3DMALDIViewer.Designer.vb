d2840c9a5d2798cc2f0de1425262654b43bc7219
xieguigang <xie.guigang@gcmodeller.org>
2022-12-10 14:39:06 +0800

    add viewer winform page

---
+++ mzkit/pages/dockWindow/documents/frm3DMALDIViewer.Designer.vb
@@ -0,0 +1,57 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm3DMALDIViewer
+    Inherits DocumentWindow
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frm3DMALDIViewer))
+        Me.WebView21 = New Microsoft.Web.WebView2.WinForms.WebView2()
+        CType(Me.WebView21, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'WebView21
+        '
+        Me.WebView21.AllowExternalDrop = True
+        Me.WebView21.CreationProperties = Nothing
+        Me.WebView21.DefaultBackgroundColor = System.Drawing.Color.White
+        Me.WebView21.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.WebView21.Location = New System.Drawing.Point(0, 0)
+        Me.WebView21.Name = "WebView21"
+        Me.WebView21.Size = New System.Drawing.Size(865, 565)
+        Me.WebView21.TabIndex = 0
+        Me.WebView21.ZoomFactor = 1.0R
+        '
+        'frm3DMALDIViewer
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(865, 565)
+        Me.Controls.Add(Me.WebView21)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frm3DMALDIViewer"
+        Me.Text = "3D MALDI Viewer"
+        CType(Me.WebView21, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents WebView21 As Microsoft.Web.WebView2.WinForms.WebView2
+End Class
---
+++ mzkit/pages/dockWindow/documents/frm3DMALDIViewer.vb
@@ -0,0 +1,77 @@
+﻿Imports System.ComponentModel
+Imports System.Threading
+Imports BioNovoGene.mzkit_win32.My
+Imports Microsoft.Web.WebView2.Core
+Imports Task
+
+Public Class frm3DMALDIViewer
+
+    Dim sourceMALDI As String
+    Dim localfs As Process
+    Dim webPort As Integer = -1
+
+    Public ReadOnly Property sourceURL As String
+        Get
+            Return $"http://127.0.0.1:{webPort}/3d-maldi.html"
+        End Get
+    End Property
+
+    Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        AutoScaleMode = AutoScaleMode.Dpi
+    End Sub
+
+    Public Sub LoadModel(maldi As String)
+        sourceMALDI = maldi
+
+    End Sub
+
+    Private Sub startHttp()
+        webPort = Net.Tcp.GetFirstAvailablePort(6001)
+        localfs = New Process With {
+            .StartInfo = New ProcessStartInfo With {
+                .FileName = $"{App.HOME}/Rstudio/bin/Rserve.exe",
+                .Arguments = $"--listen /wwwroot ""{AppEnvironment.getWebViewFolder}"" /port {webPort} --parent={App.PID}",
+                .CreateNoWindow = True,
+                .WindowStyle = ProcessWindowStyle.Hidden,
+                .UseShellExecute = True
+            }
+        }
+
+        Call localfs.Start()
+        Call App.AddExitCleanHook(Sub() Call localfs.Kill())
+    End Sub
+
+    Private Sub frm3DMALDIViewer_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        Call localfs.Kill()
+    End Sub
+
+    Private Sub WebView21_CoreWebView2InitializationCompleted(sender As Object, e As CoreWebView2InitializationCompletedEventArgs) Handles WebView21.CoreWebView2InitializationCompleted
+        Do While webPort <= 0
+            Call Application.DoEvents()
+            Call Thread.Sleep(10)
+        Loop
+
+        Call WebView21.CoreWebView2.Navigate(sourceURL)
+        Call DeveloperOptions(enable:=True)
+    End Sub
+
+    Public Sub DeveloperOptions(enable As Boolean)
+        WebView21.CoreWebView2.Settings.AreDevToolsEnabled = enable
+        WebView21.CoreWebView2.Settings.AreBrowserAcceleratorKeysEnabled = enable
+        WebView21.CoreWebView2.Settings.AreDefaultContextMenusEnabled = enable
+
+        If enable Then
+            Call MyApplication.host.showStatusMessage($"[{TabText}] WebView2 developer tools has been enable!")
+        End If
+    End Sub
+
+    Private Sub frm3DMALDIViewer_Load(sender As Object, e As EventArgs) Handles Me.Load
+        frmHtmlViewer.Init(Me.WebView21)
+        Me.TabText = Me.Text
+    End Sub
+End Class
