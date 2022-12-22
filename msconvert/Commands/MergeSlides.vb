a026059013db4767e8d7c61eee035786ccd5d411
guigang xie <xie.guigang@gcmodeller.org>
2022-12-22 10:33:57 +0800

    Update MergeSlides.vb

--- msconvert/Commands/MergeSlides.vb
+++ msconvert/Commands/MergeSlides.vb
@@ -1,8 +1,11 @@
+Imports System.Drawing
 Imports System.IO
 Imports System.Runtime.CompilerServices
 Imports BioNovoGene.Analytical.MassSpectrometry.Assembly
 Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.Comprehensive
+Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.mzData.mzWebCache
 Imports Microsoft.VisualBasic.CommandLine.InteropService.Pipeline
+Imports Microsoft.VisualBasic.Imaging.Math2D
 
 Public Module MergeSlides
 
@@ -33,8 +36,53 @@ Public Module MergeSlides
         End If
     End Function
 
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="raw"></param>
+    ''' <param name="layout">
+    ''' the grid layout, value should be whitespace or the file base name
+    ''' </param>
+    ''' <returns></returns>
     <Extension>
     Public Function MergeDataWithLayout(raw As Dictionary(Of String, mzPack), layout As String()()) As mzPack
+        Dim polygons = raw.ToDictionary(
+            Function(m) m.Key,
+            Function(m)
+                Return New Polygon2D(m.Value.MS.Select(Function(s1) s1.GetMSIPixel))
+            End Function)
+        Dim averageWidth = Aggregate p As Polygon2D In polygons.Values Into Average(p.GetRectangle.Width)
+        Dim averageHeight = Aggregate p As Polygon2D In polygons.Values Into Average(p.GetRectangle.Height)
+        Dim union As New List(Of ScanMS1)
+        Dim padding As New Size(30, 30)
+        Dim top As Integer = padding.Height
+        Dim left As Integer = padding.Width
+        Dim relativePos As Boolean = True
+        Dim norm As Boolean = True
+        Dim println As Action(Of String) = AddressOf RunSlavePipeline.SendMessage
 
+        For Each row As String() In layout
+            For Each col As String In row
+                If col.IsPattern("\s+") Then
+                    ' is a blank space, just offset the box
+                    left += padding.Width * 2 + averageWidth
+                Else
+                    Dim sample As mzPack = raw(col)
+                    Dim sample_shape = polygons(col)
+
+                    union.JoinOneSample(shape:=sample_shape, sample:=sample, left, relativePos, norm, println)
+                    left += padding.Width * 2 + (sample_shape.xpoints.Max - sample_shape.xpoints.Min)
+                End If
+            Next
+
+            ' offset the top
+            top += padding.Height + averageHeight
+        Next
+
+        Return New mzPack With {
+            .MS = union.ToArray,
+            .Application = FileApplicationClass.MSImaging,
+            .source = raw.Keys.JoinBy("+")
+        }
     End Function
 End Module
