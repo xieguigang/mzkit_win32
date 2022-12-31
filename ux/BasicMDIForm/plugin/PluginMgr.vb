f893ac99379e091a26d99b6f9b4bab8c90baf898
xieguigang <xie.guigang@gcmodeller.org>
2022-12-31 17:27:35 +0800

    fix of the registry initialization error

--- ux/BasicMDIForm/plugin/PluginMgr.vb
+++ ux/BasicMDIForm/plugin/PluginMgr.vb
@@ -13,7 +13,13 @@ Public Class PluginMgr : Inherits ListOf(Of PluginMetadata)
     Shared ReadOnly defaultLocation As String
 
     Public Sub SetStatus(id As String, status As String)
-        Dim plugin As PluginMetadata
+        Dim plugin As PluginMetadata = plugins _
+            .Where(Function(a) a.id = id) _
+            .FirstOrDefault
+
+        If Not plugin Is Nothing Then
+            plugin.status = status
+        End If
     End Sub
 
     Public Function GetPlugins() As String
@@ -41,6 +47,11 @@ Public Class PluginMgr : Inherits ListOf(Of PluginMetadata)
     End Function
 
     Public Shared Function Load() As PluginMgr
-        Return defaultLocation.LoadXml(Of PluginMgr)(throwEx:=False)
+        Dim registry = defaultLocation.LoadXml(Of PluginMgr)(throwEx:=False)
+        If registry Is Nothing Then
+            registry = New PluginMgr With {.plugins = {}}
+            registry.Save()
+        End If
+        Return registry
     End Function
 End Class
