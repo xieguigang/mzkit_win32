e35805c43c967fc406a61b0712535457bee1c749
xieguigang <xie.guigang@gcmodeller.org>
2022-12-31 14:30:00 +0800

    add plugin manager

---
+++ mzkit/pages/dockWindow/documents/frmPluginMgr.Designer.vb
@@ -0,0 +1,59 @@
+﻿Imports Mzkit_win32.BasicMDIForm
+
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmPluginMgr
+    Inherits DocumentWindow
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.WebView21 = New Microsoft.Web.WebView2.WinForms.WebView2()
+        CType(Me.WebView21, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'WebView21
+        '
+        Me.WebView21.AllowExternalDrop = True
+        Me.WebView21.CreationProperties = Nothing
+        Me.WebView21.DefaultBackgroundColor = System.Drawing.Color.White
+        Me.WebView21.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.WebView21.Location = New System.Drawing.Point(0, 0)
+        Me.WebView21.Name = "WebView21"
+        Me.WebView21.Size = New System.Drawing.Size(980, 582)
+        Me.WebView21.TabIndex = 1
+        Me.WebView21.ZoomFactor = 1.0R
+        '
+        'frmPluginMgr
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(980, 582)
+        Me.Controls.Add(Me.WebView21)
+        Me.DoubleBuffered = True
+        Me.Name = "frmPluginMgr"
+        Me.TabPageContextMenuStrip = Me.DockContextMenuStrip1
+        Me.Text = "Plugin Manager"
+        CType(Me.WebView21, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents WebView21 As Microsoft.Web.WebView2.WinForms.WebView2
+End Class
---
+++ mzkit/pages/dockWindow/documents/frmPluginMgr.vb
@@ -0,0 +1,24 @@
+﻿Imports Microsoft.Web.WebView2.Core
+Imports Mzkit_win32.BasicMDIForm
+
+Public Class frmPluginMgr
+
+    Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        AutoScaleMode = AutoScaleMode.Dpi
+    End Sub
+
+    Private Sub frmPluginMgr_Load(sender As Object, e As EventArgs) Handles Me.Load
+        WebKit.Init(WebView21)
+    End Sub
+
+    Private Sub WebView21_CoreWebView2InitializationCompleted(sender As Object, e As CoreWebView2InitializationCompletedEventArgs) Handles WebView21.CoreWebView2InitializationCompleted
+        ' WebView21.CoreWebView2.OpenDevToolsWindow()
+        Call WebView21.CoreWebView2.Navigate("")
+        Call WebKit.DeveloperOptions(WebView21, enable:=True)
+    End Sub
+End Class
