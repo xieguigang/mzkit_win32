4f2c9eb9f472233a6d723ef9958a354fa1165096
xieguigang <xie.guigang@gcmodeller.org>
2022-12-31 17:39:20 +0800

    merge controls

--- mzkit/pages/dockWindow/frmStartPage.Designer.vb
+++ mzkit/pages/dockWindow/frmStartPage.Designer.vb
@@ -78,16 +78,36 @@ Partial Class frmStartPage
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.WebView21 = New Microsoft.Web.WebView2.WinForms.WebView2()
+        CType(Me.WebView21, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'WebView21
+        '
+        Me.WebView21.AllowExternalDrop = True
+        Me.WebView21.CreationProperties = Nothing
+        Me.WebView21.DefaultBackgroundColor = System.Drawing.Color.White
+        Me.WebView21.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.WebView21.Location = New System.Drawing.Point(0, 0)
+        Me.WebView21.Name = "WebView21"
+        Me.WebView21.Size = New System.Drawing.Size(800, 450)
+        Me.WebView21.TabIndex = 1
+        Me.WebView21.ZoomFactor = 1.0R
         '
         'frmStartPage
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.WebView21)
+        Me.DoubleBuffered = True
         Me.Name = "frmStartPage"
+        Me.TabPageContextMenuStrip = Me.DockContextMenuStrip1
         Me.Text = "Start Page"
+        CType(Me.WebView21, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents WebView21 As Microsoft.Web.WebView2.WinForms.WebView2
 End Class
