c8920e5bb1d9ff036abb23f29496ec84fa6156dd
guigang xie <xie.guigang@gcmodeller.org>
2023-01-09 21:31:39 +0800

    Update TaskProgress.vb

--- ux/BasicMDIForm/buzyIndicator/TaskProgress.vb
+++ ux/BasicMDIForm/buzyIndicator/TaskProgress.vb
@@ -64,7 +64,16 @@ Imports Mzkit_win32.BasicMDIForm.CommonDialogs
 Public Interface ITaskProgress
 
     Sub SetProgressMode()
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="p">[0,100]</param>
     Sub SetProgress(p As Integer)
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="p">[0,100]</param>
+    ''' <param name="msg"></param>
     Sub SetProgress(p As Integer, msg As String)
     Sub SetTitle(title As String)
     Sub SetInfo(message As String)
