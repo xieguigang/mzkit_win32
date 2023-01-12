b35b5e9cf2b76580befd2955ee64e9e835a009a8
xieguigang <gg.xie@bionovogene.com>
2023-01-12 16:56:05 +0800

    fix of the project confligs

--- mzView/My Project/Application.Designer.vb
+++ mzView/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.mzView.Form1
+            Me.MainForm = Global.mzView.FormMain
         End Sub
     End Class
 End Namespace
