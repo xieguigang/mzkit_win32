9a0112866ff75f2925ae6c56365f30713678dd66
guigang xie <xie.guigang@gcmodeller.org>
2023-01-16 13:42:16 +0800

    make improvements of the ms-imaging data backend services abstract interface model

--- ux/BasicMDIForm/plugin/modules/MSImagingServiceModule.vb
+++ ux/BasicMDIForm/plugin/modules/MSImagingServiceModule.vb
@@ -1,6 +1,20 @@
 ﻿Public Module MSImagingServiceModule
 
-    Public StartEngine As Action
-    Public ServiceEngine As MSIServicePlugin
+    Friend m_StartEngine As Action
 
+    Public Property ServiceEngine As MSIServicePlugin
+
+    Public Sub StartEngine()
+        Call CloseEngine()
+
+        If Not m_StartEngine Is Nothing Then
+            Call m_StartEngine()
+        End If
+    End Sub
+
+    Public Sub CloseEngine()
+        If Not MSImagingServiceModule.ServiceEngine Is Nothing Then
+            Call MSImagingServiceModule.ServiceEngine.CloseEngine()
+        End If
+    End Sub
 End Module
