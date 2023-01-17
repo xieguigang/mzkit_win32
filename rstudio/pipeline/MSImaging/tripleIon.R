ca489263867f385b0b26f86dfc065b51ef073ced
guigang xie <xie.guigang@gcmodeller.org>
2023-01-17 12:19:28 +0800

    Update tripleIon.R

--- rstudio/pipeline/MSImaging/tripleIon.R
+++ rstudio/pipeline/MSImaging/tripleIon.R
@@ -9,10 +9,11 @@ imports "MsImaging" from "mzplot";
 
 options(memory.load = "max");
 
-const appPort as integer = ?"--app"    || stop("A MSimaging services hub app handle must be provided!");
-const mz as string       = ?"--mzlist" || stop("target ions list must be provided!");
-const mzdiff as string   = ?"--mzdiff" || "da:0.1";
-const savefile as string = ?"--save"   || stop("A file path to save plot image must be specificed!");
+const appPort as integer = ?"--app"     || stop("A MSimaging services hub app handle must be provided!");
+const mz as string       = ?"--mzlist"  || stop("target ions list must be provided!");
+const mzdiff as string   = ?"--mzdiff"  || "da:0.1";
+const savefile as string = ?"--save"    || stop("A file path to save plot image must be specificed!");
+const knnFill as integer = ?"--knnFill" || 3;
 const mzlist as double   = mz
 |> strsplit(",", fixed = TRUE)
 |> unlist()
