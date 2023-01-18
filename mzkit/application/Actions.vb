79a54b30b160b75001c08f0b4341748db7a86fcd
guigang xie <xie.guigang@gcmodeller.org>
2023-01-18 15:07:17 +0800

    register actions

--- mzkit/application/Actions.vb
+++ mzkit/application/Actions.vb
@@ -92,5 +92,6 @@ Module Actions
         Call Register("Peak Finding", New PeakFindingAction)
         Call Register("Peak List Annotation", New PeakAnnotationAction)
         Call Register("KEGG Stats", New KEGGStatsAction)
+        Call Register("View 3D Scatter", New ViewScatter3DAction)
     End Sub
 End Module
