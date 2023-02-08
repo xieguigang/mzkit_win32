03b3246b33356c47cb66564edeff24405ac6190e
guigang xie <xie.guigang@gcmodeller.org>
2023-02-08 11:03:31 +0800

    Create mzwork.Redis.R

---
+++ rstudio/services/mzwork.Redis.R
@@ -0,0 +1,15 @@
+imports "RedisApp" from "MZWorkRedis";
+
+#' In-memory redis database services for host mzpack objects
+#'
+
+[@info "the tcp port for run debugging in VisualStudio."]
+[@type "integer"]
+const debugPort as string  = ?"--port"   || NULL;
+[@info "the PID of the master process."]
+[@type "integer"]
+const master as string     = ?"--master" || NULL;
+
+options(memory.load = "max");
+
+RedisApp::run(as.integer(debugPort));
