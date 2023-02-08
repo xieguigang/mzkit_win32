670c666f2d728a1198a7e55ac73536c853e7ab88
guigang xie <xie.guigang@gcmodeller.org>
2023-02-08 11:38:00 +0800

    start redis service

---
+++ mzkit/application/ServiceHub/RedisService.vb
@@ -0,0 +1,38 @@
+﻿Imports BioNovoGene.mzkit_win32.ServiceHub.Manager
+Imports Microsoft.VisualBasic.ApplicationServices
+Imports Task
+Imports TaskStream
+
+''' <summary>
+''' 因为是常驻内存的数据库服务，所以在这里就只使用一个静态实例对象来开发操作接口
+''' </summary>
+Public NotInheritable Class RedisService
+
+    Public Shared Sub Start()
+        Dim Rscript As String = RscriptPipelineTask.GetRScript("../services/mzwork.Redis.R")
+        Dim port As Integer = Net.Tcp.GetFirstAvailablePort(15003)
+        Dim localRedis = New Process With {
+            .StartInfo = New ProcessStartInfo With {
+                .FileName = $"{App.HOME}/Rstudio/bin/Rscript.exe",
+                .Arguments = $"{Rscript.CLIPath} --port {port} --master {App.PID} --SetDllDirectory {TaskEngine.redisDll.ParentPath.CLIPath}",
+                .CreateNoWindow = True,
+                .WindowStyle = ProcessWindowStyle.Hidden,
+                .UseShellExecute = True
+            }
+        }
+
+        Call localRedis.Start()
+        Call App.AddExitCleanHook(Sub() Call localRedis.Kill())
+        Call Hub.Register(New Service With {
+            .CPU = 0,
+            .Name = "mzPack Redis",
+            .Description = "In-memory redis database services for view mzpack LC-MS raw data file.",
+            .isAlive = True,
+            .Memory = 0,
+            .PID = localRedis.Id,
+            .Port = port,
+            .Protocol = "TCP",
+            .StartTime = Now.ToString
+        })
+    End Sub
+End Class
--- services/mzkit_win32.envir/TaskEngine.vb
+++ services/mzkit_win32.envir/TaskEngine.vb
@@ -1,5 +1,6 @@
 ﻿Public Module TaskEngine
 
     Public ReadOnly Property hostDll As String = $"{App.HOME}/Rstudio/host/PipelineHost.dll".GetFullPath
+    Public ReadOnly Property redisDll As String = $"{App.HOME}/Rstudio/host/MZWorkRedis.dll".GetFullPath
 
 End Module
