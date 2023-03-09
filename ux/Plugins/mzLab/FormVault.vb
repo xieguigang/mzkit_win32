36ba4aa13ad3b3dc40154ab13336ed2efc58fe3e
guigang xie <xie.guigang@gcmodeller.org>
2023-03-09 16:51:59 +0800

    lazy loading

--- ux/Plugins/mzLab/FormVault.vb
+++ ux/Plugins/mzLab/FormVault.vb
@@ -38,14 +38,6 @@ Public Class FormVault
         For Each mass As MassIndex In stdlib.LoadMass
             Dim metabolite = tree.Nodes.Add(mass.name & $" [{mass.size} spectrum]")
             metabolite.Tag = mass
-
-            For Each i As Integer In mass.spectrum
-                Dim pointer = metabolite.Nodes.Add(stdlib.Libname(i))
-                pointer.Tag = i
-                pointer.ImageIndex = 1
-                pointer.SelectedImageIndex = 1
-            Next
-
             Call println(mass.ToString)
         Next
     End Sub
@@ -58,7 +50,22 @@ Public Class FormVault
     End Sub
 
     Private Sub Win7StyleTreeView1_AfterSelect(sender As Object, e As TreeViewEventArgs) Handles Win7StyleTreeView1.AfterSelect
+        Dim node = Win7StyleTreeView1.SelectedNode
 
+        If node Is Nothing OrElse node.Tag Is Nothing Then
+            Return
+        End If
+
+        If TypeOf node.Tag Is MassIndex AndAlso node.Nodes.Count = 0 Then
+            Dim mass As MassIndex = node.Tag
+
+            For Each i As Integer In mass.spectrum
+                Dim pointer = node.Nodes.Add(stdlib.Libname(i))
+                pointer.Tag = i
+                pointer.ImageIndex = 1
+                pointer.SelectedImageIndex = 1
+            Next
+        End If
     End Sub
 
     Private Sub Win7StyleTreeView1_MouseClick(sender As Object, e As MouseEventArgs) Handles Win7StyleTreeView1.MouseClick
@@ -74,7 +81,7 @@ Public Class FormVault
 
         Dim p As Integer = node.Tag
         Dim spectrum As PeakMs2 = SpectrumReader.GetSpectrum(stdlib.GetSpectrum(p))
-        Dim mat As New LibraryMatrix With {.ms2 = spectrum.mzInto, .name = $"{spectrum.lib_guid} {spectrum.mz}@{spectrum.rt}"}
+        Dim mat As New LibraryMatrix With {.ms2 = spectrum.mzInto, .name = $"{node.Text} {spectrum.lib_guid} {spectrum.mz}@{spectrum.rt}"}
         Dim img As Image = PeakAssign.DrawSpectrumPeaks(mat, size:="1920,1080").AsGDIImage
 
         Me.spectrum = spectrum
