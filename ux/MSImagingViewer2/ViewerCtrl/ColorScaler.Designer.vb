6f8669ac9c81d1cf38ea24004aa896fd260a8e5d
guigang xie <xie.guigang@gcmodeller.org>
2023-03-10 11:04:35 +0800

    implements to move the color scaler by mouse

--- ux/MSImagingViewer2/ViewerCtrl/ColorScaler.Designer.vb
+++ ux/MSImagingViewer2/ViewerCtrl/ColorScaler.Designer.vb
@@ -20,18 +20,49 @@ Partial Class ColorScaler
     '注意: 以下过程是 Windows 窗体设计器所必需的
     '可以使用 Windows 窗体设计器修改它。  
     '不要使用代码编辑器修改它。
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.picUpperbound = New System.Windows.Forms.PictureBox()
+        Me.picLowerbound = New System.Windows.Forms.PictureBox()
+        CType(Me.picUpperbound, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picLowerbound, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'picUpperbound
+        '
+        Me.picUpperbound.BackColor = System.Drawing.Color.Black
+        Me.picUpperbound.Cursor = System.Windows.Forms.Cursors.NoMoveVert
+        Me.picUpperbound.Location = New System.Drawing.Point(2, 2)
+        Me.picUpperbound.Name = "picUpperbound"
+        Me.picUpperbound.Size = New System.Drawing.Size(152, 10)
+        Me.picUpperbound.TabIndex = 0
+        Me.picUpperbound.TabStop = False
+        '
+        'picLowerbound
+        '
+        Me.picLowerbound.BackColor = System.Drawing.Color.Black
+        Me.picLowerbound.Cursor = System.Windows.Forms.Cursors.NoMoveVert
+        Me.picLowerbound.Location = New System.Drawing.Point(3, 576)
+        Me.picLowerbound.Name = "picLowerbound"
+        Me.picLowerbound.Size = New System.Drawing.Size(151, 10)
+        Me.picLowerbound.TabIndex = 1
+        Me.picLowerbound.TabStop = False
         '
         'ColorScaler
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Controls.Add(Me.picLowerbound)
+        Me.Controls.Add(Me.picUpperbound)
         Me.Name = "ColorScaler"
         Me.Size = New System.Drawing.Size(155, 589)
+        CType(Me.picUpperbound, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picLowerbound, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents picUpperbound As PictureBox
+    Friend WithEvents picLowerbound As PictureBox
 End Class
--- ux/MSImagingViewer2/ViewerCtrl/ColorScaler.vb
+++ ux/MSImagingViewer2/ViewerCtrl/ColorScaler.vb
@@ -1,3 +1,113 @@
-﻿Public Class ColorScaler
+﻿Imports System.Drawing
+Imports Microsoft.VisualBasic.ComponentModel.Ranges.Model
+Imports Microsoft.VisualBasic.Imaging.Drawing2D.Colors
 
+Public Class ColorScaler
+
+    Dim colorSet As ScalerPalette = ScalerPalette.FlexImaging
+    Dim mapLevels As Integer = 200
+
+    Public Property ScalerPalette As ScalerPalette
+        Get
+            Return colorSet
+        End Get
+        Set(value As ScalerPalette)
+            colorSet = value
+            updateColors()
+        End Set
+    End Property
+
+    Public Property ScalerLevels As Integer
+        Get
+            Return mapLevels
+        End Get
+        Set(value As Integer)
+            mapLevels = value
+            updateColors()
+        End Set
+    End Property
+
+    ''' <summary>
+    ''' range in [0,1]
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property ScalerRange As DoubleRange
+        Get
+            Dim h As Integer = Height
+            Dim up = 1 - picUpperbound.Bottom / h
+            Dim lower = 1 - picLowerbound.Top / h
+
+            Return New DoubleRange(lower, up)
+        End Get
+        Set(value As DoubleRange)
+            Dim h As Integer = Height
+            Dim upperBottom As Integer = (1 - value.Max) * h
+            Dim lowerTop As Integer = (1 - value.Min) * h
+
+            picUpperbound.Location = New Point(1, upperBottom - 10)
+            picLowerbound.Location = New Point(1, lowerTop)
+
+            updateColors()
+        End Set
+    End Property
+
+    Private Sub updateColors()
+        Dim colors As Color() = Designer.GetColors(colorSet.Description)
+
+    End Sub
+
+    Private Sub ColorScaler_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        picUpperbound.Location = New Point(1, 1)
+        picUpperbound.Size = New Size(Width - 2, 10)
+        picLowerbound.Location = New Point(1, Height - 10)
+        picLowerbound.Size = New Size(Width - 2, 10)
+    End Sub
+
+    Private Sub ColorScaler_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        picUpperbound.Size = New Size(Width - 2, 10)
+        picLowerbound.Size = New Size(Width - 2, 10)
+    End Sub
+
+    Dim moveUp, moveDown As Boolean
+    Dim mousePos As Point
+
+    Private Sub picUpperbound_MouseDown(sender As Object, e As MouseEventArgs) Handles picUpperbound.MouseDown
+        moveUp = True
+        mousePos = e.Location
+    End Sub
+
+    Private Sub picLowerbound_MouseDown(sender As Object, e As MouseEventArgs) Handles picLowerbound.MouseDown
+        moveDown = True
+        mousePos = e.Location
+    End Sub
+
+    Private Sub picUpperbound_MouseMove(sender As Object, e As MouseEventArgs) Handles picUpperbound.MouseMove
+        If moveUp Then
+            Dim oldPos = picUpperbound.Location
+            Dim delta = e.Y - mousePos.Y
+
+            mousePos = e.Location
+            picUpperbound.Location = New Point(oldPos.X, oldPos.Y + delta)
+        End If
+    End Sub
+
+    Private Sub picLowerbound_MouseMove(sender As Object, e As MouseEventArgs) Handles picLowerbound.MouseMove
+        If moveDown Then
+            Dim oldPos = picLowerbound.Location
+            Dim delta = e.Y - mousePos.Y
+
+            mousePos = e.Location
+            picLowerbound.Location = New Point(oldPos.X, oldPos.Y + delta)
+        End If
+    End Sub
+
+    Private Sub picUpperbound_MouseUp(sender As Object, e As MouseEventArgs) Handles picUpperbound.MouseUp
+        moveUp = False
+        Call updateColors()
+    End Sub
+
+    Private Sub picLowerbound_MouseUp(sender As Object, e As MouseEventArgs) Handles picLowerbound.MouseUp
+        moveDown = False
+        Call updateColors()
+    End Sub
 End Class
