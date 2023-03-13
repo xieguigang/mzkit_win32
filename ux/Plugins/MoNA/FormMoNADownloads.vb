1aaec80af5b9cdce21e563d990777d014c6a1d35
guigang xie <xie.guigang@gcmodeller.org>
2023-03-13 17:37:36 +0800

    load library from mona online services

--- ux/Plugins/MoNA/FormMoNADownloads.vb
+++ ux/Plugins/MoNA/FormMoNADownloads.vb
@@ -1,11 +1,26 @@
-﻿Public Class FormMoNADownloads
+﻿Imports Mzkit_win32.BasicMDIForm
+
+Public Class FormMoNADownloads
 
     Private Sub FormMoNADownloads_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Text = "MassBank of North America"
+        TabText = Text
+
+        Call ProgressSpinner.DoLoading(
+            Sub()
+                Call Me.Invoke(Sub() loadMoNA())
+            End Sub
+        )
+    End Sub
+
+    Private Sub loadMoNA()
         Dim libs = Library.LoadLibraries
 
         For Each libdata In libs
-            Dim libitem = ListView1.Items.Add(libdata.label)
-            libitem.SubItems.Add(libdata.description)
+            Dim libitem As ListViewItem = ListView1.Items.Add(libdata.label)
+
+            Call libitem.SubItems.Add(libdata.description)
+            Call Application.DoEvents()
         Next
     End Sub
 End Class
