454bb9747185b455c58d710adf317fa7be774f81
guigang xie <xie.guigang@gcmodeller.org>
2023-03-29 21:09:51 +0800

    add molecular network vbiewer plugin

---
+++ ux/Plugins/mzNet/FormViewer.Designer.vb
@@ -0,0 +1,72 @@
+﻿Imports Mzkit_win32.BasicMDIForm
+
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FormViewer : Inherits DocumentWindow
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TreeView1 = New System.Windows.Forms.TreeView()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TreeView1
+        '
+        Me.TreeView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TreeView1.Location = New System.Drawing.Point(0, 0)
+        Me.TreeView1.Name = "TreeView1"
+        Me.TreeView1.Size = New System.Drawing.Size(364, 722)
+        Me.TreeView1.TabIndex = 0
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.TreeView1)
+        Me.SplitContainer1.Size = New System.Drawing.Size(1094, 722)
+        Me.SplitContainer1.SplitterDistance = 364
+        Me.SplitContainer1.TabIndex = 1
+        '
+        'FormViewer
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1094, 722)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.DoubleBuffered = True
+        Me.Name = "FormViewer"
+        Me.TabPageContextMenuStrip = Me.DockContextMenuStrip1
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TreeView1 As TreeView
+    Friend WithEvents SplitContainer1 As SplitContainer
+End Class
---
+++ ux/Plugins/mzNet/FormViewer.vb
@@ -0,0 +1,19 @@
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math.MoleculeNetworking.PoolData
+
+Public Class FormViewer
+
+    Dim tree As HttpTreeFs
+
+    Private Sub FormViewer_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim root = TreeView1.Nodes.Add("Spectrum Pool")
+
+        Me.tree = New HttpTreeFs("http://192.168.0.207:83/taxonomy")
+
+        Dim childs = Me.tree.GetTreeChilds("/").ToArray
+
+        For Each dir As String In childs
+            Dim node = root.Nodes.Add(dir.BaseName)
+            node.Tag = dir
+        Next
+    End Sub
+End Class
