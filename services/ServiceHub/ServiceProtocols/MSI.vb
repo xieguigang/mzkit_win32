3ec25363672ba3c2d404766bdbcaada5638a4129
guigang xie <xie.guigang@gcmodeller.org>
2023-05-04 15:02:19 +0800

    fix of the byte size read error

--- services/ServiceHub/ServiceProtocols/MSI.vb
+++ services/ServiceHub/ServiceProtocols/MSI.vb
@@ -288,9 +288,18 @@ Public Class MSI : Implements ITaskDriver, IDisposable
            .CreateObject(Of RegionLoader)(decodeMetachar:=False) _
            .Reload
         Dim allPixels As PixelScan() = MSI.pixelReader.AllPixels.ToArray
-        Dim targets = allPixels.Where(Function(i) regions.ContainsPixel(i.X, i.Y)).Select(Function(i) i.GetMs).IteratesALL.ToArray
+        Dim targets As ms2() = allPixels _
+            .AsParallel _
+            .Where(Function(i) regions.ContainsPixel(i.X, i.Y)) _
+            .Select(Function(i) i.GetMs) _
+            .IteratesALL _
+            .ToArray
         Dim ms1 As ms2() = targets.Centroid(Tolerance.DeltaMass(0.3), New RelativeIntensityCutoff(0.05))
-        Dim mat As New LibraryMatrix With {.name = regions.sample_tags.JoinBy("; "), .centroid = True, .ms2 = ms1}
+        Dim mat As New LibraryMatrix With {
+            .name = regions.sample_tags.JoinBy("; "),
+            .centroid = True,
+            .ms2 = ms1
+        }
 
         Return New DataPipe(mat.GetStream)
     End Function
