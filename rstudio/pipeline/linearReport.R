e56501a31df3f10dc6830d83cd591983a92bd8bc
xieguigang <xie.guigang@gcmodeller.org>
2023-05-16 15:32:09 +0800

    fix of the report script

--- rstudio/pipeline/linearReport.R
+++ rstudio/pipeline/linearReport.R
@@ -5,8 +5,9 @@ imports 'Linears' from 'mz_quantify';
 
 const packfile    = ?"--linear" || stop("A linear data file is required!");
 const export_html = ?"--export" || `${dirname(packfile)}/${basename(packfile)}_report.html`;
-const linearPack  = read.linearPack(packfile);
+const linearPack  = read.linearPack(packfile) |> linear.setErrPoints();
 const standards   = [linearPack]::linears;
+const ionsRaw     = linear.ions_raw(linearPack);
 
 standards 
 |> report.dataset(NULL, NULL, ionsRaw = ionsRaw)
