6283a3d90930ec975596fce76585cba3f1cbbc93
xieguigang <xie.guigang@gcmodeller.org>
2023-05-17 20:14:52 +0800

    configs for run ST-imaging test

--- rstudio/test/convert_h5ad_st_to_mzpack.R
+++ rstudio/test/convert_h5ad_st_to_mzpack.R
@@ -1,5 +1,6 @@
 
 imports "BackgroundTask" from "PipelineHost";
+imports "STImaging" from "PipelineHost";
 imports "package_utils" from "devkit";
 
 package_utils::attach("D:\Erica");
@@ -10,11 +11,11 @@ require(mzkit);
 
 imports "mzweb" from "mzkit";
 
-spots = read.spatial_spots("F:\20221117-ST\tissue_positions_list.csv");
-matrix = read.ST_spacerangerH5Matrix("F:\20221117-ST\raw_feature_bc_matrix.h5");
+spots = read.spatial_spots("C:\Users\lipidsearch\Desktop\tissue_positions_list.csv");
+matrix = read.ST_spacerangerH5Matrix("C:\Users\lipidsearch\Desktop\raw_feature_bc_matrix.h5");
 
 str(spots);
 str(matrix);
 
 ST_spaceranger.mzpack(spots, matrix)
-|> write.mzPack(file = "F:\20221117-ST\tissue_positions_list.mzPack");
+|> write.mzPack(file = "C:\Users\lipidsearch\Desktop\raw_feature_bc_matrix.mzPack");
