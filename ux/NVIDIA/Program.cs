b798241b76e8851fc35717e00e57d55ad2878d55
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-05-30 18:26:45 +0800

    add project

---
+++ ux/NVIDIA/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace nvidia_anselAI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
