78fa7b2bc8ae5b11034a671ad28e3b3a04540da4
xieguigang <gg.xie@bionovogene.com>
2023-05-31 19:05:30 +0800

    fix of the missing parameters

--- ux/Plugins/mzNet/FormViewer.vb
+++ ux/Plugins/mzNet/FormViewer.vb
@@ -80,7 +80,8 @@ Public Class FormViewer
         Dim key As String = If(node.Contains("/"), HttpTreeFs.ClusterHashIndex(node), node)
         Dim getMetadata As PoolData.Metadata() = HttpRESTMetadataPool.FetchClusterData(
             url_get:=$"{cloud.tree.HttpServices}/get/metadata/",
-            hash_index:=key
+            hash_index:=key,
+            model_id:=1
         ).ToArray
 
         Call LoadTable(
