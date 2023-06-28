8fd4d62747793898160e79e6b452a3ae500e7e69
xieguigang <gg.xie@bionovogene.com>
2023-06-28 09:41:51 +0800

    input cluster similarity score

---
+++ mzkit/forms/Inputs/LCMS/InputMNSimilarityScore.vb
@@ -0,0 +1,20 @@
+﻿Imports Microsoft.VisualBasic.Windows.Forms.DataValidation
+
+Public Class InputMNSimilarityScore
+
+    Public ReadOnly Property GetCutoff As Double
+        Get
+            Return Val(TextBox1.Text)
+        End Get
+    End Property
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        DialogResult = DialogResult.Cancel
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.ValidateDouble("cluster similarity") IsNot Nothing Then
+            DialogResult = DialogResult.OK
+        End If
+    End Sub
+End Class
