6dd1f14e758e4ed8adeb400c65c968c172827161
xieguigang <gg.xie@bionovogene.com>
2023-06-29 11:47:42 +0800

    select and view spectrum graph models

--- ux/Plugins/mzNet/InputSelectGraphModel.Designer.vb
+++ ux/Plugins/mzNet/InputSelectGraphModel.Designer.vb
@@ -24,16 +24,133 @@ Partial Class InputSelectGraphModel
     '不要使用代码编辑器修改它。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox1.Controls.Add(Me.Button3)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.ComboBox1)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(374, 283)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Spectrum Graph Model"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox2.Location = New System.Drawing.Point(17, 163)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ReadOnly = True
+        Me.TextBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox2.Size = New System.Drawing.Size(343, 109)
+        Me.TextBox2.TabIndex = 4
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(17, 137)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(343, 20)
+        Me.ComboBox1.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(15, 114)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(161, 12)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "View Spectrum Graph Model:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(16, 53)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(344, 21)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(15, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(107, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Connect To Cloud:"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(278, 301)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "OK"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(197, 301)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Cancel"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(243, 80)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(108, 23)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "Load Models"
+        Me.Button3.UseVisualStyleBackColor = True
         '
         'InputSelectGraphModel
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(581, 321)
+        Me.ClientSize = New System.Drawing.Size(398, 333)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Name = "InputSelectGraphModel"
         Me.Text = "Select Spectrum Graph Model"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
 End Class
--- ux/Plugins/mzNet/InputSelectGraphModel.vb
+++ ux/Plugins/mzNet/InputSelectGraphModel.vb
@@ -1,4 +1,39 @@
-﻿Public Class InputSelectGraphModel
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math.MoleculeNetworking.PoolData
+Imports Microsoft.VisualBasic.My.JavaScript
+
+Public Class InputSelectGraphModel
+
+    Const default_resource As String = "http://novocell.mzkit.org:83/taxonomy"
+
+    Public ReadOnly Property GetModel As SpectrumGraphModel
+        Get
+            Return ComboBox1.SelectedItem
+        End Get
+    End Property
+
+    Private Sub InputSelectGraphModel_Load(sender As Object, e As EventArgs) Handles Me.Load
+        If Not CheckCloudConnection($"{default_resource}/get_models/") Then
+            MessageBox.Show("The default cloud resource is not avaiable at now, it may be move to a new network location, you could specific the new cloud service location and then try again.", "Cloud Services not avaiable", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        Else
+            TextBox1.Text = default_resource
+        End If
+
+        Call loadModelList()
+    End Sub
+
+    Private Sub loadModelList()
+        Dim list As JavaScriptObject() = Restful.ParseJSON($"{TextBox1.Text}/get_models/".GET).info
+        Dim models = list.Select(Function(js) js.CreateObject(Of SpectrumGraphModel)).ToArray
+
+        Call ComboBox1.Items.Clear()
+
+        For Each graph As SpectrumGraphModel In models
+            Call ComboBox1.Items.Add(graph)
+        Next
+
+        ComboBox1.SelectedIndex = 0
+    End Sub
 
     ''' <summary>
     ''' Check the cloud services is avaiable or not?
@@ -6,6 +41,42 @@
     ''' <param name="urlCloudSvr"></param>
     ''' <returns></returns>
     Public Shared Function CheckCloudConnection(urlCloudSvr As String) As Boolean
+        Try
+            Dim e404 As Boolean = False
 
+            ' try to get remote cloud service response
+            Call urlCloudSvr.GET(is404:=e404, timeoutSec:=3)
+
+            If e404 Then
+                Return False
+            Else
+                Return True
+            End If
+        Catch ex As Exception
+            Return False
+        End Try
     End Function
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Call loadModelList()
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        TextBox2.Text = DirectCast(ComboBox1.SelectedValue, SpectrumGraphModel).description
+    End Sub
+End Class
+
+Public Class SpectrumGraphModel
+
+    Public Property add_time As Date
+    Public Property description As String
+    Public Property flag As Integer
+    Public Property id As Integer
+    Public Property name As String
+    Public Property parameters As String
+
+    Public Overrides Function ToString() As String
+        Return $"[{id}] {name}"
+    End Function
+
 End Class
