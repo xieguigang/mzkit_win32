22a01f48098efee8d2605bd1de31129376422804
guigang xie <xie.guigang@gcmodeller.org>
2023-07-06 17:16:43 +0800

    how to rendering the scatter image in fast?

--- ux/LCMSViewer/PeakScatterViewer.Designer.vb
+++ ux/LCMSViewer/PeakScatterViewer.Designer.vb
@@ -22,8 +22,17 @@ Partial Class PeakScatterViewer
     '不要使用代码编辑器修改它。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.SuspendLayout()
+        '
+        'PeakScatterViewer
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Name = "PeakScatterViewer"
+        Me.Size = New System.Drawing.Size(815, 608)
+        Me.ResumeLayout(False)
+
     End Sub
 
 End Class
--- ux/LCMSViewer/PeakScatterViewer.vb
+++ ux/LCMSViewer/PeakScatterViewer.vb
@@ -1,7 +1,85 @@
-﻿
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math.Spectra.Xml
+Imports Microsoft.VisualBasic.ComponentModel.Algorithm
+Imports Microsoft.VisualBasic.ComponentModel.Ranges.Model
+Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Imaging.Drawing2D.Colors
+
 ''' <summary>
 ''' mz@rt 2d scatter viewer
 ''' </summary>
 Public Class PeakScatterViewer
 
+    Public Event ClickOnPeak(peakId As String, mz As Double, rt As Double)
+    Public Event MoveOverPeak(peakId As String, mz As Double, rt As Double)
+
+    Public Property ColorScale As ScalerPalette = ScalerPalette.turbo
+    Public Property ScaleLevels As Integer = 255
+
+    Dim mzscale As d3js.scale.LinearScale
+    Dim rtscale As d3js.scale.LinearScale
+
+    Dim mz_range As DoubleRange
+    Dim rt_range As DoubleRange
+
+    Dim mzBins As BlockSearchFunction(Of Meta)
+    Dim rtBins As BlockSearchFunction(Of Meta)
+
+    Private Sub PeakScatterViewer_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Public Function LoadPeaks(peaksdata As IEnumerable(Of Meta)) As PeakScatterViewer
+        Dim rawdata As Meta() = peaksdata.ToArray
+
+        mzBins = New BlockSearchFunction(Of Meta)(rawdata, Function(i) i.mz, 0.5, fuzzy:=True)
+        rtBins = New BlockSearchFunction(Of Meta)(rawdata, Function(i) i.scan_time, 5, fuzzy:=True)
+        mz_range = New DoubleRange(rawdata.Select(Function(i) i.mz))
+        rt_range = New DoubleRange(rawdata.Select(Function(i) i.scan_time))
+
+        Call Rendering()
+        Call ViewerResize()
+
+        Return Me
+    End Function
+
+    ''' <summary>
+    ''' rendering of the scatter image
+    ''' </summary>
+    Private Sub Rendering()
+
+    End Sub
+
+    Private Sub PeakScatterViewer_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
+        Dim peakId As String = Nothing
+        Dim mz, rt As Double
+
+        Call GetPeak(peakId, mz, rt)
+
+        RaiseEvent MoveOverPeak(peakId, mz, rt)
+    End Sub
+
+    Private Sub GetPeak(ByRef peakId As String, ByRef mz As Double, ByRef rt As Double)
+        Dim pt = PointToClient([Control].MousePosition)
+        Dim size As Size = Me.Size
+
+        mz = (pt.X / size.Width) * mz_range.Length + mz_range.Min
+        rt = (pt.Y / size.Height) * rt_range.Length + rt_range.Min
+        peakId = Nothing
+    End Sub
+
+    Private Sub ViewerResize() Handles Me.Resize
+        Dim size As Size = Me.Size
+
+        mzscale = d3js.scale.linear().domain(mz_range).range(integers:={0, size.Width})
+        rtscale = d3js.scale.linear().domain(rt_range).range(integers:={0, size.Height})
+    End Sub
+
+    Private Sub PeakScatterViewer_MouseClick(sender As Object, e As MouseEventArgs) Handles Me.MouseClick
+        Dim peakId As String = Nothing
+        Dim mz, rt As Double
+
+        Call GetPeak(peakId, mz, rt)
+
+        RaiseEvent ClickOnPeak(peakId, mz, rt)
+    End Sub
 End Class
