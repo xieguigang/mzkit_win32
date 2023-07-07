cca8c8220421a965e1e14af1d1bc39142f74f39f
guigang xie <xie.guigang@gcmodeller.org>
2023-07-07 09:38:08 +0800

    open UI

--- mzkit/pages/dockWindow/documents/frmLCMSScatterViewer.vb
+++ mzkit/pages/dockWindow/documents/frmLCMSScatterViewer.vb
@@ -1,4 +1,5 @@
 ﻿Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.MZWork
+Imports Mzkit_win32.BasicMDIForm
 
 Public Class frmLCMSScatterViewer
 
@@ -12,10 +13,11 @@ Public Class frmLCMSScatterViewer
         Me.raw.LoadMzpack(Sub(src, cache) frmFileExplorer.getRawCache(src,, cache))
         Me.TabText = raw.source.FileName
 
-
+        Call Workbench.AppHost.SetTitle($"LCMS Scatter '{raw.source.FileName}'")
     End Sub
 
     Private Sub frmLCMSScatterViewer_Load(sender As Object, e As EventArgs) Handles Me.Load
+        ToolStripStatusLabel1.Text = "Ready!"
         ApplyVsTheme(StatusStrip1)
     End Sub
 End Class
