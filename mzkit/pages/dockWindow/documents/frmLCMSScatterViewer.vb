16993af061e27f45f6a9766e5931fff58467564b
guigang xie <xie.guigang@gcmodeller.org>
2023-07-07 09:36:23 +0800

    open LCMS scatter viewer from raw file

---
+++ mzkit/pages/dockWindow/documents/frmLCMSScatterViewer.vb
@@ -0,0 +1,21 @@
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Assembly.MZWork
+
+Public Class frmLCMSScatterViewer
+
+    ''' <summary>
+    ''' 已经加载的Raw文件
+    ''' </summary>
+    Dim raw As Raw
+
+    Public Sub loadRaw(raw As Raw)
+        Me.raw = raw
+        Me.raw.LoadMzpack(Sub(src, cache) frmFileExplorer.getRawCache(src,, cache))
+        Me.TabText = raw.source.FileName
+
+
+    End Sub
+
+    Private Sub frmLCMSScatterViewer_Load(sender As Object, e As EventArgs) Handles Me.Load
+        ApplyVsTheme(StatusStrip1)
+    End Sub
+End Class
