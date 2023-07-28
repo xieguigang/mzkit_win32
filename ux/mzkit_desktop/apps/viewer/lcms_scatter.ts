734e364392c0a7b7657308a7b8bf83b1f2965c50
guigang xie <xie.guigang@gcmodeller.org>
2023-07-28 15:02:07 +0800

    Update lcms_scatter.ts

--- ux/mzkit_desktop/apps/viewer/lcms_scatter.ts
+++ ux/mzkit_desktop/apps/viewer/lcms_scatter.ts
@@ -13,46 +13,53 @@ namespace apps.viewer {
             return "lcms-scatter";
         }
 
+        private renderer: THREE.WebGLRenderer;
+        private camera: THREE.PerspectiveCamera;
+        private scene: THREE.Scene;
+
+        private get canvas(): HTMLCanvasElement {
+            return this.renderer.domElement;
+        }
+
         protected init(): void {
+            const vm = this;
+
+            this.renderer = new THREE.WebGLRenderer({
+                antialias: true
+            });
+            this.camera = new THREE.PerspectiveCamera(60, 1, 1, 1000);
+            this.scene = new THREE.Scene();
+            this.camera.position.setScalar(150);
+
             app.desktop.mzkit.GetLCMSScatter().then(async function (data) {
                 const json_str: string = await data;
                 const scatter: ms1_scatter[] = JSON.parse(json_str);
 
                 if (isNullOrEmpty(scatter)) {
-                    LCMSScatterViewer.render3DScatter([]);
+                    vm.render3DScatter([]);
                 } else {
-                    LCMSScatterViewer.render3DScatter(scatter);
+                    vm.render3DScatter(scatter);
                 }
             });
         }
 
-        public static render3DScatter(dataset: ms1_scatter[]) {
-            // @author prisoner849
+        public render3DScatter(dataset: ms1_scatter[]) {
+            var canvas = this.renderer.domElement;
+            var camera = this.camera;
 
-            var scene = new THREE.Scene();
-            var camera = new THREE.PerspectiveCamera(60, 1, 1, 1000);
-            camera.position.setScalar(150);
-            var renderer = new THREE.WebGLRenderer({
-                antialias: true
-            });
-            var canvas = renderer.domElement;
             document.body.appendChild(canvas);
 
             var controls = new THREE.OrbitControls(camera, canvas);
-
             var light = new THREE.DirectionalLight(0xffffff, 1.5);
-            light.position.setScalar(100);
-            scene.add(light);
-            scene.add(new THREE.AmbientLight(0xffffff, 0.5));
 
-            var size = { x: 200, y: 200 };
-            var pointsCount = 1000;
-            var points3d = [];
-            for (let i = 0; i < pointsCount; i++) {
-                let x = THREE.Math.randFloatSpread(size.x);
-                let z = THREE.Math.randFloatSpread(size.y);
-                let y = noise.perlin2(x / size.x * 5, z / size.y * 5) * 50;
-                points3d.push(new THREE.Vector3(x, y, z));
+            light.position.setScalar(100);
+            this.scene.add(light);
+            this.scene.add(new THREE.AmbientLight(0xffffff, 0.5));
+
+            var points3d: THREE.Vector3[] = [];
+
+            for (let p of dataset) {
+                points3d.push(new THREE.Vector3(p.mz, p.scan_time, p.intensity));
             }
 
             var geom = new THREE.BufferGeometry().setFromPoints(points3d);
@@ -60,7 +67,7 @@ namespace apps.viewer {
                 geom,
                 new THREE.PointsMaterial({ color: 0x99ccff, size: 2 })
             );
-            scene.add(cloud);
+            this.scene.add(cloud);
 
             // triangulate x, z
             var indexDelaunay = Delaunator.from(
@@ -80,32 +87,39 @@ namespace apps.viewer {
                 geom, // re-use the existing geometry
                 new THREE.MeshLambertMaterial({ color: "purple", wireframe: true })
             );
-            scene.add(mesh);
+            this.scene.add(mesh);
 
             var gui = new dat.GUI();
             gui.add(mesh.material, "wireframe");
 
-            render();
+            this.render();
+        }
 
-            function resize(renderer) {
-                const canvas = renderer.domElement;
-                const width = canvas.clientWidth;
-                const height = canvas.clientHeight;
-                const needResize = canvas.width !== width || canvas.height !== height;
-                if (needResize) {
-                    renderer.setSize(width, height, false);
-                }
-                return needResize;
+        private render() {
+            const canvas = this.canvas;
+            const camera = this.camera;
+
+            if (this.resize(this.renderer)) {
+                camera.aspect = canvas.clientWidth / canvas.clientHeight;
+                camera.updateProjectionMatrix();
             }
 
-            function render() {
-                if (resize(renderer)) {
-                    camera.aspect = canvas.clientWidth / canvas.clientHeight;
-                    camera.updateProjectionMatrix();
-                }
-                renderer.render(scene, camera);
-                requestAnimationFrame(render);
+            this.renderer.render(this.scene, camera);
+
+            requestAnimationFrame(() => this.render());
+        }
+
+        private resize(renderer: THREE.WebGLRenderer) {
+            const canvas = renderer.domElement;
+            const width = canvas.clientWidth;
+            const height = canvas.clientHeight;
+            const needResize = canvas.width !== width || canvas.height !== height;
+
+            if (needResize) {
+                renderer.setSize(width, height, false);
             }
+
+            return needResize;
         }
     }
 }
