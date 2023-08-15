37f608a0c434c8e5d065b7975056a379345efcb4
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-08-15 19:55:28 +0800

    config mzblender background services

--- mzblender/Protocol.vb
+++ mzblender/Protocol.vb
@@ -1,4 +1,18 @@
 ﻿Public Enum Protocol
-
+    ''' <summary>
+    ''' Open a new session and load new MSI pixel layer data
+    ''' </summary>
+    OpenSession
+    ''' <summary>
+    ''' Rendering a new MS-imaging layer based on new parameters
+    ''' </summary>
+    MSIRender
+    ''' <summary>
+    ''' Destroy the current MSI blender session
+    ''' </summary>
+    Destroy
+    ''' <summary>
+    ''' Shutdown the blender server
+    ''' </summary>
     Shutdown
 End Enum
