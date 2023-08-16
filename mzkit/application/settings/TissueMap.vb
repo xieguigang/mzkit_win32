cc31c323310e4039f26ecb8570730d4bb3b06dd7
guigang xie <xie.guigang@gcmodeller.org>
2023-08-16 16:54:31 +0800

    move configs data into the msi viewer project

--- mzkit/application/settings/TissueMap.vb
+++ mzkit/application/settings/TissueMap.vb
@@ -1,4 +1,4 @@
-﻿Imports Microsoft.VisualBasic.Imaging
+﻿Imports Mzkit_win32.MSImagingViewerV2.PolygonEditor
 
 Namespace Configuration
 
@@ -20,24 +20,4 @@ Namespace Configuration
         End Function
     End Class
 
-    Public Class PolygonEditorConfigs
-
-        Public Property point_size As Single
-        Public Property point_color As String
-        Public Property show_points As Boolean
-        Public Property line_width As Single
-        Public Property dash As Boolean
-        Public Property line_color As String
-
-        Public Shared Function GetDefault() As PolygonEditorConfigs
-            Return New PolygonEditorConfigs With {
-                .dash = False,
-                .line_color = Color.Black.ToHtmlColor,
-                .line_width = 3,
-                .point_color = Color.Red.ToHtmlColor,
-                .point_size = 8,
-                .show_points = True
-            }
-        End Function
-    End Class
 End Namespace
---
+++ ux/MSImagingViewer2/MSI/Editor/Configs.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Drawing
+
+Namespace PolygonEditor
+
+    Public Class PolygonEditorConfigs
+
+        Public Property point_size As Single
+        Public Property point_color As String
+        Public Property show_points As Boolean
+        Public Property line_width As Single
+        Public Property dash As Boolean
+        Public Property line_color As String
+
+        Public Shared Function GetDefault() As PolygonEditorConfigs
+            Return New PolygonEditorConfigs With {
+                .dash = False,
+                .line_color = Color.Black.ToHtmlColor,
+                .line_width = 3,
+                .point_color = Color.Red.ToHtmlColor,
+                .point_size = 8,
+                .show_points = True
+            }
+        End Function
+    End Class
+End Namespace
