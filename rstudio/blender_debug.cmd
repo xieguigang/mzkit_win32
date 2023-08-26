73181c61f652f6863ac6d00e648d36cb3c9d32fe
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-08-26 15:17:56 +0800

    Update blender_debug.cmd

--- rstudio/blender_debug.cmd
+++ rstudio/blender_debug.cmd
@@ -1,7 +1,7 @@
 @echo off
 
-set blender=plugins/blender/BlenderHost.exe 
+set blender=./plugins/blender/BlenderHost.exe 
 set workbench=mzkit_win32
 
-%workbench% --debug --port=33361 --blender=8331
-%blender% /start --port 8331 --master debug-blender
+REM /b %workbench% --debug --port=33361 --blender=8331
+"%blender%" /start --port 8331 --master debug-blender --debug
