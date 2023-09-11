f8398ea8516c7709a1895509f2e79cbe0c82eb58
guigang xie <xie.guigang@gcmodeller.org>
2023-09-11 11:30:50 +0800

    andalso confogs of the adducts list

--- ux/Plugins/mzNet/frmSetClusterNumbers.Designer.vb
+++ ux/Plugins/mzNet/frmSetClusterNumbers.Designer.vb
@@ -25,41 +25,32 @@ Partial Class frmSetClusterNumbers
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.CheckedListBox1 = New System.Windows.Forms.CheckedListBox()
         Me.GroupBox1.SuspendLayout()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'GroupBox1
         '
-        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.CheckedListBox1)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.NumericUpDown1)
+        Me.GroupBox1.Controls.Add(Me.ComboBox1)
+        Me.GroupBox1.Controls.Add(Me.Label2)
         Me.GroupBox1.Controls.Add(Me.Label1)
         Me.GroupBox1.Location = New System.Drawing.Point(6, 7)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(242, 100)
+        Me.GroupBox1.Size = New System.Drawing.Size(242, 264)
         Me.GroupBox1.TabIndex = 0
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Set Arguments"
-        '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(168, 115)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 1
-        Me.Button1.Text = "OK"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
-        'Button2
-        '
-        Me.Button2.Location = New System.Drawing.Point(86, 115)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
-        Me.Button2.TabIndex = 2
-        Me.Button2.Text = "Cancel"
-        Me.Button2.UseVisualStyleBackColor = True
         '
         'Label1
         '
@@ -70,26 +61,83 @@ Partial Class frmSetClusterNumbers
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "TopN Spectrum Clusters:"
         '
-        'TextBox1
+        'Button1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(18, 59)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(137, 21)
-        Me.TextBox1.TabIndex = 1
-        Me.TextBox1.Text = "100"
+        Me.Button1.Location = New System.Drawing.Point(168, 278)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "OK"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(86, 278)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Cancel"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(18, 90)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(83, 12)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Set Ion Mode:"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Positive", "Negative"})
+        Me.ComboBox1.Location = New System.Drawing.Point(116, 87)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(110, 20)
+        Me.ComboBox1.TabIndex = 3
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(18, 55)
+        Me.NumericUpDown1.Maximum = New Decimal(New Integer() {10000, 0, 0, 0})
+        Me.NumericUpDown1.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(208, 21)
+        Me.NumericUpDown1.TabIndex = 4
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {1000, 0, 0, 0})
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(18, 120)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(95, 12)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Select Adducts:"
+        '
+        'CheckedListBox1
+        '
+        Me.CheckedListBox1.FormattingEnabled = True
+        Me.CheckedListBox1.Location = New System.Drawing.Point(18, 138)
+        Me.CheckedListBox1.Name = "CheckedListBox1"
+        Me.CheckedListBox1.Size = New System.Drawing.Size(208, 116)
+        Me.CheckedListBox1.TabIndex = 6
         '
         'frmSetClusterNumbers
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(255, 148)
+        Me.ClientSize = New System.Drawing.Size(255, 309)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.GroupBox1)
         Me.Name = "frmSetClusterNumbers"
-        Me.Text = "Load TopN Clusters"
+        Me.Text = "Load Spectrum Clusters"
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox1.PerformLayout()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -97,6 +145,10 @@ Partial Class frmSetClusterNumbers
     Friend WithEvents GroupBox1 As GroupBox
     Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
-    Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Label1 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents CheckedListBox1 As CheckedListBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents NumericUpDown1 As NumericUpDown
 End Class
--- ux/Plugins/mzNet/frmSetClusterNumbers.vb
+++ ux/Plugins/mzNet/frmSetClusterNumbers.vb
@@ -1,8 +1,31 @@
-﻿Public Class frmSetClusterNumbers
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math.Ms1.PrecursorType
+Imports std = System.Math
+
+Public Class frmSetClusterNumbers
 
     Public ReadOnly Property TopNClusters As Integer
         Get
-            Return CInt(Val(TextBox1.Text))
+            Return CInt(NumericUpDown1.Value)
+        End Get
+    End Property
+
+    Public ReadOnly Property GetIonMode As IonModes
+        Get
+            Return Provider.ParseIonMode(ComboBox1.SelectedValue.ToString)
+        End Get
+    End Property
+
+    Public ReadOnly Property GetAdducts As MzCalculator()
+        Get
+            Dim list As New List(Of MzCalculator)
+
+            For i As Integer = 0 To CheckedListBox1.Items.Count - 1
+                If CheckedListBox1.GetItemChecked(i) Then
+                    Call list.Add(CheckedListBox1.Items(i))
+                End If
+            Next
+
+            Return list.ToArray
         End Get
     End Property
 
@@ -19,5 +42,32 @@
         End If
 
         Me.DialogResult = DialogResult.OK
+    End Sub
+
+    Private Sub frmSetClusterNumbers_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ComboBox1.SelectedIndex = 0
+        UpdateAdducts()
+    End Sub
+
+    Private Sub UpdateAdducts()
+        Dim adducts As MzCalculator()
+
+        Call CheckedListBox1.Items.Clear()
+
+        If GetIonMode = IonModes.Positive Then
+            adducts = Provider.Positives
+        Else
+            adducts = Provider.Negatives
+        End If
+
+        For Each item As MzCalculator In adducts
+            Call CheckedListBox1.Items.Add(item)
+
+            If std.Abs(item.charge) = 1 AndAlso item.M = 1 Then
+                Call CheckedListBox1.SetItemChecked(CheckedListBox1.Items.Count - 1, True)
+            Else
+                Call CheckedListBox1.SetItemChecked(CheckedListBox1.Items.Count - 1, False)
+            End If
+        Next
     End Sub
 End Class
