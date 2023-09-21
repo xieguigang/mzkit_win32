5c9bf6545938adf9af2da029657b07e701643aa7
guigang xie <xie.guigang@gcmodeller.org>
2023-09-21 14:56:28 +0800

    add library

---
+++ services/BioDeep/LocalLibrary/RQLib.vb
@@ -0,0 +1,16 @@
+﻿Imports System.IO
+Imports Microsoft.VisualBasic.DataStorage.HDSPack.FileSystem
+Imports RQL
+
+''' <summary>
+''' Model for local library file for supports the metabolite 
+''' annotation on ms1/ms2 level
+''' </summary>
+Public Class RQLib
+
+    ReadOnly query As Resource
+
+    Sub New(file As Stream)
+        query = New Resource(New StreamPack(file))
+    End Sub
+End Class
