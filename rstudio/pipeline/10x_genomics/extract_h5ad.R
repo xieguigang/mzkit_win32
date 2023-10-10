e31bcbe0b71211123a2da30e43fe81815f2b820d
guigang xie <xie.guigang@gcmodeller.org>
2023-10-10 15:05:08 +0800

    handling of the possible file content error

--- rstudio/pipeline/10x_genomics/extract_h5ad.R
+++ rstudio/pipeline/10x_genomics/extract_h5ad.R
@@ -3,7 +3,7 @@ require(mzkit);
 require(JSON);
 
 imports "singleCell" from "Erica";
-imports "mzweb" from "mzkit";
+imports ["mzweb", "TissueMorphology"] from "mzkit";
 imports "BackgroundTask" from "PipelineHost";
 imports "STImaging" from "PipelineHost";
 
@@ -11,31 +11,38 @@ const h5ad_file as string  = ?"--h5ad" || stop("The 10x genomics h5ad rawdata fi
 const out_file as string   = ?"--save" || `${dirname(h5ad_file)}/${basename(h5ad_file)}.mzPack`;
 const export_dir as string = dirname(out_file);
 const raw = h5ad_file |> read.h5ad(loadExpr0 = TRUE);
-const spatial = raw |> spatialMap(useCellAnnotation = TRUE);
+const export_spatial = function(raw) {
+    const spatial = raw |> spatialMap(useCellAnnotation = TRUE);
 
-print("peeks of the spatial data:");
-print(spatial, max.print = 6);
+    print("peeks of the spatial data:");
+    print(spatial, max.print = 6);    
 
-write.csv(spatial, file = `${export_dir}/spatial.csv`, row.names = FALSE);
+    bitmap(file = `${export_dir}/spatial_3.png`) {
+        plot(spatial[, "x"], spatial[, "y"],
+            padding      = "padding:200px 500px 200px 250px;",
+            class        = spatial[, "class"],
+            title        = "Spatial 2D Tissue Map",
+            x.lab        = "X",
+            y.lab        = "Y",
+            legend.block = 100,
+            point.size   = 6,
+            colorSet     = "paper",  
+            grid.fill    = "transparent",
+            size         = [2800, 3200],
+            x.format     = "F0",
+            y.format     = "F0",
+            reverse      = TRUE
+        );
+    }
 
-bitmap(file = `${export_dir}/spatial_3.png`) {
-	plot(spatial[, "x"], spatial[, "y"],
-		 padding      = "padding:200px 500px 200px 250px;",
-		 class        = spatial[, "class"],
-		 title        = "Spatial 2D Tissue Map",
-		 x.lab        = "X",
-		 y.lab        = "Y",
-		 legend.block = 100,
-		 point.size   = 6,
-		 colorSet     = "paper",  
-		 grid.fill    = "transparent",
-		 size         = [2800, 3200],
-		 x.format     = "F0",
-		 y.format     = "F0",
-		 reverse      = TRUE
-	);
+    write.csv(spatial, file = `${export_dir}/spatial.csv`, row.names = FALSE);
 }
+
+try({
+    export_spatial(raw);
+});
 
 const [gene_exprs, tissue] = extract_h5ad(raw);
 
 write.mzPack(gene_exprs, file = savefile);
+TissueMorphology::writeCDF(tissue, file = `${export_dir}/${basename(out_file)}.cdf`);
