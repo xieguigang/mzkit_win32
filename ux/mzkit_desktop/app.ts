252a96ef2e0574bebbebf7108536531aa5aa34a2
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-11-04 11:17:27 +0800

    create umap analysis page

--- ux/mzkit_desktop/app.ts
+++ ux/mzkit_desktop/app.ts
@@ -59,7 +59,7 @@ namespace app.desktop {
         Router.AddAppHandler(new apps.viewer.clusterViewer());
         Router.AddAppHandler(new apps.viewer.LCMSScatterViewer());
         Router.AddAppHandler(new apps.viewer.OpenseadragonSlideViewer());
-
+        Router.AddAppHandler(new apps.viewer.umap());
         Router.AddAppHandler(new apps.viewer.lcmsLibrary());
 
         Router.RunApp();
---
+++ ux/mzkit_desktop/apps/viewer/umap.ts
@@ -0,0 +1,14 @@
+namespace apps.viewer {
+
+    export class umap extends Bootstrap {
+
+        public get appName(): string {
+            return "umap";
+        }
+
+        protected init(): void {
+
+        }
+
+    }
+}
