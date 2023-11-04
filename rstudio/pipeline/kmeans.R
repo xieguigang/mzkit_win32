7efa03959e3693d36e09754c637ee1cb8430ff00
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-11-05 01:27:49 +0800

    renames class label

--- rstudio/pipeline/kmeans.R
+++ rstudio/pipeline/kmeans.R
@@ -6,6 +6,10 @@ const savefile = ?"--save" || `${dirname(input_data)}/${basename(input_data)}_km
 
 const rawdata = read.csv(input_data, row.names = 1, check.names = FALSE);
 const clusters = kmeans(rawdata, centers = k) |> as.data.frame();
+const class = clusters$Cluster;
+
+clusters[,"Cluster"] = NULL;
+clusters[,"class"] = class;
 
 print("get k-means result:");
 print(clusters, max.print = 13);
