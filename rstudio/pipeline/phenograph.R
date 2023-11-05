09568a649ca288e31787e762efdd6471b9fe6b97
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-11-05 10:56:21 +0800

    Update phenograph.R

--- rstudio/pipeline/phenograph.R
+++ rstudio/pipeline/phenograph.R
@@ -1,13 +1,32 @@
-imports "BackgroundTask" from "PipelineHost";
+require(GCModeller);
+require(Erica);
+
 imports "igraph" from "igraph";
+imports "phenograph" from "Erica";
+imports "geneExpression" from "phenotype_kit";
 
-const raw as string       = ?"--raw"     || stop("missing of the raw data file for run MSI data analysis!");
-const topN as integer     = ?"--top"     || 10;
-const maxDims as integer  = ?"--maxDims" || 256;
-const mzErr as string     = ?"--mzdiff"  || "da:0.3";
-const savegraph as string = ?"--save"    || `${dirname(raw)}/${basename(raw)}_phenograph/`; 
+[@info "the matrix file path for run the phenograph cluster analysis."]
+const input_data as string = ?"--raw"      || stop("missing of the raw data matrix file for run MSI data analysis!");
+const read_bin as boolean = ?"--read_bin" || FALSE;
+const knn as integer = as.integer(?"--knn" || 16);
+[@info "the directory path for export the graph result"]
+const savegraph as string = ?"--save"     || `${dirname(raw)}/${basename(raw)}_phenograph/`; 
 
-raw
-|> phenograph(topN, maxDims, mzErr)
-|> save.network(file = savegraph)
+const read_data = function() {
+	if (!read_bin) {
+		load.expr(input_data);
+	} else {	
+		input_data
+		|> load.expr0(lazy = FALSE)
+		;
+	}
+}
+
+let pheno = read_data()
+|> phenograph(k = knn, score = phenograph::score_metric("cosine"))
 ;
+
+print(as.data.frame(V(pheno)));
+print(as.data.frame(E(pheno)));
+
+save.network(pheno, file = savegraph);
