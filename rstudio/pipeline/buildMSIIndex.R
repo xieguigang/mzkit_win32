38837d433e3d289d05cb84ac9b230b5c297be512
guigang xie <xie.guigang@gcmodeller.org>
2023-12-22 14:33:31 +0800

    running pipeline in batch mode

--- rstudio/pipeline/buildMSIIndex.R
+++ rstudio/pipeline/buildMSIIndex.R
@@ -3,14 +3,43 @@ imports "BackgroundTask" from "PipelineHost";
 # title: Build indexed MSI cache
 # author: xieguigang <xie.guigang@gcmodeller.org>
 
-[@info "the file path of the imzML raw data file to create cache index."]
+[@info "the file path of the imzML raw data file to create cache index, or a valid direcotry 
+        path that contains multiple imzML rawdata files if running in batch mode."]
 [@type "*.imzML"]
 const imzML as string = ?"--imzML" || stop("no raw data file provided!");
-[@info "the file path of the MSI indexed cache file."]
-[@type "filepath"]
-const cache as string = ?"--cache" || stop("a cache file path must be provided!");
+[@info "running the conversion in batch mode? this parameter required of the input ``--imzML`` 
+        source parameter should be a directory path, and the export ``--cache`` path also 
+        could be a directory path if it has been specificed."]
+const in_batch as boolean = ?"--batch" || FALSE;
 [@info "intensity cutoff for removes noise data."]
 const cutoff as double = ?"--into_cutoff" || 0.0;
 
+[@info "the file path of the MSI indexed cache file."]
+[@type "filepath"]
+const cache as string = {
+    if (!in_batch) {
+        (?"--cache" || stop("a cache file path must be provided!"));
+    } else {
+        # data file will be export to the source directory
+        # if this cache output parameter has not been specificed
+        (?"--cache" || imzML);
+    }
+};
+
 sleep(1);
-BackgroundTask::cache.MSI(imzML, cache, cutoff);
+
+if (in_batch) {
+    let rawfiles = list.files(imzML, pattern = ["*.imzML"]);
+
+    print(`get ${length(rawfiles)} imzML source inputs:`);
+    print(basename(rawfiles));
+
+    for(file in rawfiles) {
+        BackgroundTask::cache.MSI(
+            file, `${imzML}/${basename(file)}.mzPack`, 
+            cutoff
+        );
+    }
+} else {
+    BackgroundTask::cache.MSI(imzML, cache, cutoff);
+}
