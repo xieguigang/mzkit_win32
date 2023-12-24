ab6ae93b34de52589a4083138689ba02b8dd721c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-12-24 10:49:47 +0800

    needs a wizard dialog for imports lcms peaktable

---
+++ mzkit/forms/Inputs/LCMS/InputImportsPeaktableDialog.Designer.vb
@@ -0,0 +1,39 @@
+﻿Imports Mzkit_win32.BasicMDIForm.CommonDialogs
+
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class InputImportsPeaktableDialog
+    Inherits InputDialog
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SuspendLayout()
+        '
+        'InputImportsPeaktableDialog
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(889, 595)
+        Me.Name = "InputImportsPeaktableDialog"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+End Class
---
+++ mzkit/forms/Inputs/LCMS/InputImportsPeaktableDialog.vb
@@ -0,0 +1,3 @@
+﻿Public Class InputImportsPeaktableDialog
+
+End Class
