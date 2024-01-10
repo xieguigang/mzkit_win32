2896be6573872490d9d5bcb5387fb45bd8d51e60
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-10 22:07:49 +0800

    add table data

--- mzkit/pages/dockWindow/documents/frmMetabonomicsAnalysis.vb
+++ mzkit/pages/dockWindow/documents/frmMetabonomicsAnalysis.vb
@@ -12,7 +12,28 @@ Public Class frmMetabonomicsAnalysis
     Dim metadata As New Dictionary(Of String, JavaScriptObject)
 
     Public Sub LoadData(table As DataTable)
+        Dim groups = sampleinfo.GroupBy(Function(s) s.sample_info) _
+            .Select(Function(g) (g.Key, list:=g.ToArray)) _
+            .ToArray
 
+        Call table.Columns.Add("xcms_id", GetType(String))
+
+        For Each group In groups
+            Call table.Columns.Add(group.Key, GetType(Double))
+        Next
+
+        For Each peak As xcms2 In peaks.peaks
+            Dim row As Object() = New Object(groups.Length) {}
+            row(0) = peak.ID
+
+            For i As Integer = 0 To groups.Length - 1
+                Dim group = groups(i).list
+                Dim data As Double() = peak(group.Select(Function(s) s.ID))
+                row(i + 1) = data.Average
+            Next
+
+            table.Rows.Add(row)
+        Next
     End Sub
 
     Public Sub LoadData(table As File, title As String)
@@ -59,6 +80,45 @@ Public Class frmMetabonomicsAnalysis
         Me.peaks = New PeakSet With {
             .peaks = peaks.ToArray
         }
+
+        Call loadTable()
+    End Sub
+
+    Dim memoryData As System.Data.DataSet
+
+    Private Sub loadTable()
+        memoryData = New System.Data.DataSet
+
+        Dim table As DataTable = memoryData.Tables.Add("memoryData")
+
+        Try
+            Call Me.AdvancedDataGridView1.Columns.Clear()
+            Call Me.AdvancedDataGridView1.Rows.Clear()
+        Catch ex As Exception
+
+        End Try
+
+        Call LoadData(table)
+        Call AdvancedDataGridView1.SetDoubleBuffered()
+
+        'For Each column As DataGridViewColumn In AdvancedDataGridView1.Columns
+        '    'Select Case table.Columns.Item(column.HeaderText).DataType
+        '    '    Case GetType(String)
+        '    '        AdvancedDataGridView1.SetSortEnabled(column, True)
+        '    '    Case GetType(Double)
+        '    '    Case GetType(Integer)
+        '    '    Case Else
+        '    '        ' do nothing 
+        '    'End Select
+
+        '    AdvancedDataGridView1.ShowMenuStrip(column)
+        'Next
+
+        BindingSource1.DataSource = memoryData
+        BindingSource1.DataMember = table.TableName
+
+        AdvancedDataGridView1.DataSource = BindingSource1
+        AdvancedDataGridViewSearchToolBar1.SetColumns(AdvancedDataGridView1.Columns)
     End Sub
 
 End Class
