ae3203f39759784aef2fe33262c1bc19fff744b1
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-11 13:26:41 +0800

    load spectrum data

--- ux/LCMSViewer/SpectrumViewer.Designer.vb
+++ ux/LCMSViewer/SpectrumViewer.Designer.vb
@@ -22,8 +22,55 @@ Partial Class SpectrumViewer
     '不要使用代码编辑器修改它。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.PictureBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(1048, 607)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 607)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(1048, 22)
+        Me.StatusStrip1.TabIndex = 1
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(119, 17)
+        Me.ToolStripStatusLabel1.Text = "ToolStripStatusLabel1"
+        '
+        'SpectrumViewer
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Name = "SpectrumViewer"
+        Me.Size = New System.Drawing.Size(1048, 629)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
 End Class
--- ux/LCMSViewer/SpectrumViewer.vb
+++ ux/LCMSViewer/SpectrumViewer.vb
@@ -1,3 +1,51 @@
-﻿Public Class SpectrumViewer
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math
+Imports BioNovoGene.Analytical.MassSpectrometry.Math.Spectra
+Imports Microsoft.VisualBasic.ComponentModel.Algorithm
+Imports Microsoft.VisualBasic.ComponentModel.Ranges.Model
+Imports Microsoft.VisualBasic.Imaging
 
+Public Class SpectrumViewer
+
+    Dim spectrum As ms2()
+    Dim highlights As Integer
+    Dim mzscale As d3js.scale.LinearScale
+    Dim intensityscale As d3js.scale.LinearScale
+
+    Dim mz_index As BlockSearchFunction(Of (mz As Double, Integer))
+    Dim mz_range As DoubleRange
+    Dim into_range As DoubleRange
+
+    Public Property title As String = "MS Spectrum"
+
+    Public Sub SetSpectrum(spec As LibraryMatrix)
+        spectrum = spec.ms2
+        title = If(spec.name, "MS Spectrum")
+        highlights = -1
+        mz_range = spectrum.Select(Function(m) m.mz).Range
+        into_range = spectrum.Select(Function(m) m.intensity).Range
+        mz_index = spectrum.CreateMzIndex()
+
+        Call Scaling()
+        Call Rendering()
+    End Sub
+
+    Private Sub SpectrumViewer_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        Call Scaling()
+        Call Rendering()
+    End Sub
+
+    Private Sub Scaling()
+        mzscale = d3js.scale.linear().domain(mz_range).range(New Double() {0, PictureBox1.Width})
+        intensityscale = d3js.scale.linear().domain(into_range).range(New Double() {0, PictureBox1.Height})
+    End Sub
+
+    Private Sub Rendering()
+        Using g As Graphics2D = PictureBox1.Size.CreateGDIDevice(filled:=BackColor)
+
+        End Using
+    End Sub
+
+    Private Sub PictureBox1_MouseMove(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseMove
+        Dim pos = e.Location
+    End Sub
 End Class
