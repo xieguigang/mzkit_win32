6753ed36401d0aaa297dfb5948bd4b8ce1834880
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-12 17:26:05 +0800

    plsda function

---
+++ rstudio/pipeline/workbench/PCA.R
@@ -0,0 +1,25 @@
+require(GCModeller);
+
+#' the gene expression matrix data toolkit
+imports "geneExpression" from "phenotype_kit";
+
+let mat = ?"--matrix" || stop("a data matrix file must be provided!");
+let ncomp as integer = ?"--ncomp" || 3;
+let outputdir = ?"--outputdir" || dirname(mat);
+
+let matrix = mat 
+|> load.expr0(
+    lazy = FALSE) 
+|> geneExpression::tr()
+|> as.data.frame()
+;
+
+# str(matrix);
+
+let pca = prcomp(matrix, pc = ncomp);
+
+print(pca);
+
+write.csv(pca$score, file = `${outputdir}/pca_score.csv`);
+write.csv(pca$loading, file = `${outputdir}/pca_loading.csv`);
+writeLines(pca$contribution, con = `${outputdir}/pca_contrib.txt`);
