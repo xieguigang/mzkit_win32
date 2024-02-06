d803b89f3fc484dd7efec0ca389a48c519249431
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-02-06 10:23:40 +0800

    Update PCA.R

--- rstudio/pipeline/workbench/PCA.R
+++ rstudio/pipeline/workbench/PCA.R
@@ -1,10 +1,12 @@
 require(GCModeller);
+require(ggplot);
 
 imports "stats" from "Rlapack";
 #' the gene expression matrix data toolkit
 imports "geneExpression" from "phenotype_kit";
 
 let mat = ?"--matrix" || stop("a data matrix file must be provided!");
+let sampleinfo = ?"--sampleinfo" || stop("the sample class label file must be provided!");
 let ncomp as integer = ?"--ncomp" || 3;
 let outputdir = ?"--outputdir" || dirname(mat);
 
@@ -14,6 +16,11 @@ let matrix = mat
 |> geneExpression::tr()
 |> as.data.frame()
 ;
+let sample_info = read.csv(sampleinfo, row.names = 1, check.names = FALSE);
+let class_id = as.list(sample_info, byrow = TRUE) |> lapply(x -> x$sample_info);
+
+print(sample_info);
+str(class_id);
 
 # str(matrix);
 
@@ -24,3 +31,17 @@ print(pca);
 write.csv(pca$score, file = `${outputdir}/pca/pca_score.csv`);
 write.csv(pca$loading, file = `${outputdir}/pca/pca_loading.csv`);
 writeLines(pca$contribution, con = `${outputdir}/pca/pca_contrib.txt`);
+
+let pca_score = pca$score;
+
+pca_score[, "class_id"] = sapply(rownames(pca_score), x -> class_id[[x]]);
+
+svg(file = `${outputdir}/pca/pca_score.svg`) {
+    ggplot(pca_score, aes(x="PC1", y = "PC2", color = "class_id", label = rownames(pca_score)))
+    + geom_point(
+        size = 9
+    )
+    + geom_text(size = 6)
+    # + stat_ellipse()
+    ;
+}
