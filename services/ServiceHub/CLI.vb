18161bf8db2784b080e461f6d05c139d9f9f6ba3
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-21 15:55:49 +0800

    needs a better test tool

---
+++ services/ServiceHub/CLI.vb
@@ -0,0 +1,10 @@
+﻿Imports Microsoft.VisualBasic.CommandLine.InteropService.SharedORM
+
+<CLI>
+Module CLI
+
+    Public Function Main() As Integer
+        Return GetType(CLI).RunCLI(App.CommandLine)
+    End Function
+
+End Module
