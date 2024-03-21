165a28bfeb67338605e50ceeecdbf335d5becd53
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-21 16:36:28 +0800

    test data loader

--- services/test/msi_test.R
+++ services/test/msi_test.R
@@ -1,3 +1,4 @@
 imports "debugger" from "ServiceHub";
 
 let msi_app = debugger::load_session("\\192.168.1.254\backup3\项目以外内容\客户测试项目交付\mzkit_test\union\union.mzPack");
+let layers = debugger::load_summary_layer(msi_app);
