586d581885557d521c074d9db74c622c631773bd
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-21 16:29:07 +0800

    configs memory for tyest

--- services/ServiceHub/Debugger.vb
+++ services/ServiceHub/Debugger.vb
@@ -2,11 +2,16 @@
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Net.Protocols.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports Microsoft.VisualBasic.My
 
 <Package("debugger")>
 Module Debugger
 
     ReadOnly debug_local As IPEndPoint = New Microsoft.VisualBasic.Net.IPEndPoint("127.0.0.1", 0)
+
+    Sub New()
+        Call FrameworkInternal.ConfigMemory(FrameworkInternal.MemoryLoads.Max)
+    End Sub
 
     <ExportAPI("load_session")>
     Public Function loadSession(raw As String) As ProtocolHandler
