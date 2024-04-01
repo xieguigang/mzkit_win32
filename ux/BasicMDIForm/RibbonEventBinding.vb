e2245fd94afe3e3f146781f5747d087df1b1ac59
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-04-01 14:01:46 +0800

    make improvements of the ribbon menu helper

--- ux/BasicMDIForm/RibbonEventBinding.vb
+++ ux/BasicMDIForm/RibbonEventBinding.vb
@@ -1,7 +1,6 @@
-﻿Imports Mzkit_win32.BasicMDIForm
-Imports RibbonLib.Controls
+﻿Imports RibbonLib.Controls
 
-Public Class RibbonEventBinding
+Public Class RibbonEventBinding : Implements IDisposable
 
     ReadOnly ribbon As RibbonButton
 
@@ -9,10 +8,21 @@ Public Class RibbonEventBinding
     ''' binding an instance event to static ribbon event
     ''' </summary>
     Public evt As Action
+    Private disposedValue As Boolean
+
+    Public ReadOnly Property HasHookEvent As Boolean
+        Get
+            Return Not evt Is Nothing
+        End Get
+    End Property
 
     Sub New(btn As RibbonButton)
         ribbon = btn
         AddHandler ribbon.ExecuteEvent, Sub() Call exec_call()
+    End Sub
+
+    Public Sub ClearHook()
+        evt = Nothing
     End Sub
 
     Private Sub exec_call()
@@ -33,4 +43,29 @@ Public Class RibbonEventBinding
         Return ribbon.ToString
     End Function
 
+    Protected Overridable Sub Dispose(disposing As Boolean)
+        If Not disposedValue Then
+            If disposing Then
+                ' TODO: 释放托管状态(托管对象)
+                Call ClearHook()
+            End If
+
+            ' TODO: 释放未托管的资源(未托管的对象)并重写终结器
+            ' TODO: 将大型字段设置为 null
+            disposedValue = True
+        End If
+    End Sub
+
+    ' ' TODO: 仅当“Dispose(disposing As Boolean)”拥有用于释放未托管资源的代码时才替代终结器
+    ' Protected Overrides Sub Finalize()
+    '     ' 不要更改此代码。请将清理代码放入“Dispose(disposing As Boolean)”方法中
+    '     Dispose(disposing:=False)
+    '     MyBase.Finalize()
+    ' End Sub
+
+    Public Sub Dispose() Implements IDisposable.Dispose
+        ' 不要更改此代码。请将清理代码放入“Dispose(disposing As Boolean)”方法中
+        Dispose(disposing:=True)
+        GC.SuppressFinalize(Me)
+    End Sub
 End Class
