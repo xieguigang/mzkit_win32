af616531d5d8339144fc11162eb9571ba3d56564
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-04-19 13:08:43 +0800

    plot mass windows

--- mzkit/forms/Inputs/LCMS/InputSelectMassWindow.Designer.vb
+++ mzkit/forms/Inputs/LCMS/InputSelectMassWindow.Designer.vb
@@ -1,6 +1,8 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿Imports Mzkit_win32.BasicMDIForm.CommonDialogs
+
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class InputSelectMassWindow
-    Inherits System.Windows.Forms.Form
+    Inherits InputDialog
 
     'Form 重写 Dispose，以清理组件列表。
     <System.Diagnostics.DebuggerNonUserCode()> _
@@ -23,9 +25,9 @@ Partial Class InputSelectMassWindow
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        Me.Label1 = New System.Windows.Forms.Label()
         Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.GroupBox1.SuspendLayout()
@@ -39,40 +41,40 @@ Partial Class InputSelectMassWindow
         Me.GroupBox1.Controls.Add(Me.PictureBox1)
         Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(761, 449)
+        Me.GroupBox1.Size = New System.Drawing.Size(493, 438)
         Me.GroupBox1.TabIndex = 0
         Me.GroupBox1.TabStop = False
-        Me.GroupBox1.Text = "GroupBox1"
-        '
-        'PictureBox1
-        '
-        Me.PictureBox1.Location = New System.Drawing.Point(15, 32)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(725, 205)
-        Me.PictureBox1.TabIndex = 0
-        Me.PictureBox1.TabStop = False
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(37, 261)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(59, 12)
-        Me.Label1.TabIndex = 1
-        Me.Label1.Text = "m/z list:"
+        Me.GroupBox1.Text = "Mass Windows"
         '
         'ListBox1
         '
         Me.ListBox1.FormattingEnabled = True
         Me.ListBox1.ItemHeight = 12
-        Me.ListBox1.Location = New System.Drawing.Point(15, 295)
+        Me.ListBox1.Location = New System.Drawing.Point(8, 281)
         Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(443, 136)
+        Me.ListBox1.Size = New System.Drawing.Size(316, 148)
         Me.ListBox1.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 256)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(59, 12)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "m/z list:"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(8, 20)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(477, 222)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(650, 479)
+        Me.Button1.Location = New System.Drawing.Point(422, 461)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 1
@@ -81,7 +83,7 @@ Partial Class InputSelectMassWindow
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(555, 479)
+        Me.Button2.Location = New System.Drawing.Point(327, 461)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(75, 23)
         Me.Button2.TabIndex = 2
@@ -92,12 +94,12 @@ Partial Class InputSelectMassWindow
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(785, 523)
+        Me.ClientSize = New System.Drawing.Size(517, 494)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.GroupBox1)
         Me.Name = "InputSelectMassWindow"
-        Me.Text = "Form1"
+        Me.Text = "Select m/z bin"
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox1.PerformLayout()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
--- mzkit/forms/Inputs/LCMS/InputSelectMassWindow.vb
+++ mzkit/forms/Inputs/LCMS/InputSelectMassWindow.vb
@@ -1,3 +1,38 @@
-﻿Public Class InputSelectMassWindow
+﻿Imports BioNovoGene.Analytical.MassSpectrometry.Math
+Imports BioNovoGene.Analytical.MassSpectrometry.Math.Ms1
+Imports BioNovoGene.Analytical.MassSpectrometry.Visualization
+Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Canvas
+Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Imaging.Driver
+Imports Mzkit_win32.BasicMDIForm.CommonDialogs
+
+Public Class InputSelectMassWindow
+
+    Public Shared Sub GetMassWindows(mz As IEnumerable(Of Double), apply As Action(Of MassWindow))
+        Dim histogram = MzBins.GetScatter(mz, 0.005)
+        Dim windows = MzBins.GetMzBins(histogram.x, histogram.y).ToArray
+        Dim selector As New InputSelectMassWindow
+        Dim theme As New Theme
+        Dim plot As New PlotMassWindow(histogram.x, histogram.y, windows, theme)
+        Dim size As Size = (selector.PictureBox1.Size)
+
+        size = New Size(size.Width * 5, size.Height * 5)
+        selector.PictureBox1.BackgroundImage = plot _
+            .Plot(size, dpi:=200, Drivers.GDI) _
+            .AsGDIImage
+
+        For Each item As MassWindow In windows
+            Call selector.ListBox1.Items.Add(item)
+        Next
+
+        Call InputDialog.Input(
+            setConfig:=Sub(val)
+                           Dim i As Integer = selector.ListBox1.SelectedIndex
+                           Dim mass As MassWindow = windows(i)
+
+                           Call apply(mass)
+                       End Sub,
+            config:=selector)
+    End Sub
 
 End Class
