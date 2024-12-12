94c521eb82a8e58397b95de816ba5f67743dc23c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-12-12 20:44:17 +0800

    implements of the bitmap conversion

--- mzblender/GdiInterop.vb
+++ mzblender/GdiInterop.vb
@@ -1,60 +1,63 @@
 ﻿#Region "Microsoft.VisualBasic::4b6c716a5d92cdab44ec4fc7da3d7580, mzkit\mzblender\GdiInterop.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 48
-    '    Code Lines: 42 (87.50%)
-    ' Comment Lines: 0 (0.00%)
-    '    - Xml Docs: 0.00%
-    ' 
-    '   Blank Lines: 6 (12.50%)
-    '     File Size: 1.15 KB
+' Summaries:
 
 
-    ' Module GdiInterop
-    ' 
-    '     Function: (+2 Overloads) CTypeFromGdiImage, (+2 Overloads) CTypeGdiImage
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 48
+'    Code Lines: 42 (87.50%)
+' Comment Lines: 0 (0.00%)
+'    - Xml Docs: 0.00%
+' 
+'   Blank Lines: 6 (12.50%)
+'     File Size: 1.15 KB
+
+
+' Module GdiInterop
+' 
+'     Function: (+2 Overloads) CTypeFromGdiImage, (+2 Overloads) CTypeGdiImage
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.Drawing.Imaging
+Imports System.IO
 Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Imaging
 
 #If NET48 Then
 Imports Image = System.Drawing.Image
@@ -80,7 +83,12 @@ Module GdiInterop
 #If NET48 Then
         Return bitmap
 #Else
-        Throw New NotImplementedException
+        Using ms As New MemoryStream
+            Call bitmap.Save(ms, ImageFormats.Bmp)
+            Call ms.Seek(Scan0, SeekOrigin.Begin)
+
+            Return System.Drawing.Bitmap.FromStream(ms)
+        End Using
 #End If
     End Function
 
@@ -98,7 +106,12 @@ Module GdiInterop
 #If NET48 Then
         Return bitmap
 #Else
-        Throw New NotImplementedException
+        Using ms As New MemoryStream
+            Call bitmap.Save(ms, ImageFormat.Bmp)
+            Call ms.Seek(Scan0, SeekOrigin.Begin)
+
+            Return Microsoft.VisualBasic.Imaging.Bitmap.FromStream(ms)
+        End Using
 #End If
     End Function
 End Module
