cdad4b3dd092380157d7680a9c6b749d5f5a9bf9
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-12-19 13:46:45 +0800

    fix of the namespace

--- services/mzkit_win32.blender_argvs/PlotProperty.vb
+++ services/mzkit_win32.blender_argvs/PlotProperty.vb
@@ -70,135 +70,138 @@ Imports Microsoft.VisualBasic.MIME.Html.CSS
 Imports Microsoft.VisualBasic.Serialization.JSON
 Imports any = Microsoft.VisualBasic.Scripting
 
-Public Class PlotProperty
+Namespace InternalBlender
 
-    <Category("Plot")> <Description("The width of the plot image in pixels")> Public Property width As Integer = 2048
-    <Category("Plot")> <Description("The height of the plot image in pixels")> Public Property height As Integer = 1600
+    Public Class PlotProperty
 
-    <Category("Plot")> <Description("The background color of the plot image")> Public Property background As Color = Color.White
-    <Category("Plot")> <Description("Text of the plot its main title")> Public Property title As String
-    <Category("Plot")> <Description("Text of the plot its legend title")> Public Property legend_title As String
-    <Category("Plot")> <Description("Text label of the X axis")> Public Property xlabel As String = "X"
-    <Category("Plot")> <Description("Text label of the Y axis")> Public Property ylabel As String = "Y"
+        <Category("Plot")> <Description("The width of the plot image in pixels")> Public Property width As Integer = 2048
+        <Category("Plot")> <Description("The height of the plot image in pixels")> Public Property height As Integer = 1600
 
-    <Category("Padding")> <DisplayName("top")> Public Property padding_top As Integer = 100
-    <Category("Padding")> <DisplayName("left")> Public Property padding_left As Integer = 100
-    <Category("Padding")> <DisplayName("right")> Public Property padding_right As Integer = 100
-    <Category("Padding")> <DisplayName("bottom")> Public Property padding_bottom As Integer = 100
+        <Category("Plot")> <Description("The background color of the plot image")> Public Property background As Color = Color.White
+        <Category("Plot")> <Description("Text of the plot its main title")> Public Property title As String
+        <Category("Plot")> <Description("Text of the plot its legend title")> Public Property legend_title As String
+        <Category("Plot")> <Description("Text label of the X axis")> Public Property xlabel As String = "X"
+        <Category("Plot")> <Description("Text label of the Y axis")> Public Property ylabel As String = "Y"
 
-    <Category("Styles: legends")> <Description("Show data legend on the plot image?")> Public Property show_legend As Boolean = True
-    <Category("Styles: legends")> <Description("divide the legends element into multiple block group by this number.")> Public Property legend_block_size As Integer = 16
-    <Category("Styles")> <Description("Show data grid of the data plot in the background?")> Public Property show_grid As Boolean = True
-    <Category("Styles")> <Description("Show data tag label for the interested object of the data plot?")> Public Property show_tag As Boolean = True
-    <Category("Styles")> <Description("Fill the curve plot?")> Public Property fill_curve As Boolean = True
-    <Category("Styles")> <Description("Tweaks of the line width of the data line plot.")> Public Property line_width As Single = 5
-    <Category("Styles")> <Description("Tweaks of the point size of the data scatter plot.")> Public Property point_size As Single = 10
-    <Category("Styles")> Public Property label_font As Font = defaultCss.GetFont(CSSFont.Win10NormalLarger)
-    <Category("Styles: legends")> Public Property legend_font As Font = defaultCss.GetFont(CSSFont.Win10NormalLarge)
-    <Category("Styles")> Public Property axis_label_font As Font = defaultCss.GetFont(CSSFont.Win10NormalLarge)
-    <Category("Styles")> <Description("Tweaks of the fill color of the grid background.")> Public Property gridFill As Color = "rgb(245,245,245)".TranslateColor
-    <Category("Styles")> Public Property colors As CategoryPalettes = CategoryPalettes.ColorBrewerSet1
+        <Category("Padding")> <DisplayName("top")> Public Property padding_top As Integer = 100
+        <Category("Padding")> <DisplayName("left")> Public Property padding_left As Integer = 100
+        <Category("Padding")> <DisplayName("right")> Public Property padding_right As Integer = 100
+        <Category("Padding")> <DisplayName("bottom")> Public Property padding_bottom As Integer = 100
 
-    ''' <summary>
-    ''' 自定义颜色列表
-    ''' </summary>
-    ''' <returns></returns>
-    <Browsable(False)> Public Property colorSet As String
+        <Category("Styles: legends")> <Description("Show data legend on the plot image?")> Public Property show_legend As Boolean = True
+        <Category("Styles: legends")> <Description("divide the legends element into multiple block group by this number.")> Public Property legend_block_size As Integer = 16
+        <Category("Styles")> <Description("Show data grid of the data plot in the background?")> Public Property show_grid As Boolean = True
+        <Category("Styles")> <Description("Show data tag label for the interested object of the data plot?")> Public Property show_tag As Boolean = True
+        <Category("Styles")> <Description("Fill the curve plot?")> Public Property fill_curve As Boolean = True
+        <Category("Styles")> <Description("Tweaks of the line width of the data line plot.")> Public Property line_width As Single = 5
+        <Category("Styles")> <Description("Tweaks of the point size of the data scatter plot.")> Public Property point_size As Single = 10
+        <Category("Styles")> Public Property label_font As Font = defaultCss.GetFont(CSSFont.Win10NormalLarger)
+        <Category("Styles: legends")> Public Property legend_font As Font = defaultCss.GetFont(CSSFont.Win10NormalLarge)
+        <Category("Styles")> Public Property axis_label_font As Font = defaultCss.GetFont(CSSFont.Win10NormalLarge)
+        <Category("Styles")> <Description("Tweaks of the fill color of the grid background.")> Public Property gridFill As Color = "rgb(245,245,245)".TranslateColor
+        <Category("Styles")> Public Property colors As CategoryPalettes = CategoryPalettes.ColorBrewerSet1
 
-    Shared ReadOnly defaultCss As CSSEnvirnment = CSSEnvirnment.Empty
+        ''' <summary>
+        ''' 自定义颜色列表
+        ''' </summary>
+        ''' <returns></returns>
+        <Browsable(False)> Public Property colorSet As String
 
-    Public Function GetTheme() As Theme
-        Return New Theme With {
-            .padding = $"padding: {padding_top}px {padding_right}px {padding_bottom}px {padding_left}px;",
-            .colorSet = GetColorSetName(),
-            .pointSize = point_size,
-            .drawGrid = show_grid,
-            .background = background.ToHtmlColor,
-            .drawLegend = show_legend,
-            .tagCSS = New CSSFont(label_font).ToString,
-            .legendLabelCSS = New CSSFont(legend_font).ToString,
-            .axisLabelCSS = New CSSFont(axis_label_font).ToString,
-            .gridFill = gridFill.ToHtmlColor,
-            .legendSplitSize = legend_block_size
-        }
-    End Function
+        Shared ReadOnly defaultCss As CSSEnvirnment = CSSEnvirnment.Empty
 
-    Public Function GetColorSetName() As String
-        If colors = CategoryPalettes.NA Then
-            Return colorSet
-        Else
-            Return colors.Description
-        End If
-    End Function
+        Public Function GetTheme() As Theme
+            Return New Theme With {
+                .padding = $"padding: {padding_top}px {padding_right}px {padding_bottom}px {padding_left}px;",
+                .colorSet = GetColorSetName(),
+                .pointSize = point_size,
+                .drawGrid = show_grid,
+                .background = background.ToHtmlColor,
+                .drawLegend = show_legend,
+                .tagCSS = New CSSFont(label_font).ToString,
+                .legendLabelCSS = New CSSFont(legend_font).ToString,
+                .axisLabelCSS = New CSSFont(axis_label_font).ToString,
+                .gridFill = gridFill.ToHtmlColor,
+                .legendSplitSize = legend_block_size
+            }
+        End Function
 
-    Public Function GetPadding() As Padding
-        Return New Padding(padding_left, padding_top, padding_right, padding_bottom)
-    End Function
-
-    Shared ReadOnly css_data As PropertyInfo() = GetType(PlotProperty) _
-        .GetProperties(DataFramework.PublicProperty) _
-        .Where(Function(p) p.GetIndexParameters.IsNullOrEmpty) _
-        .ToArray
-
-    Public Function GetJSON() As String
-        Dim json As New Dictionary(Of String, String)
-
-        For Each p As PropertyInfo In css_data
-            Dim val As Object = p.GetValue(Me)
-            Dim str As String
-
-            If val Is Nothing Then
-                str = Nothing
-            ElseIf TypeOf val Is Color Then
-                str = DirectCast(val, Color).ToHtmlColor
-            ElseIf TypeOf val Is Font Then
-                str = New CSSFont(DirectCast(val, Font)).CSSValue
-            ElseIf DataFramework.IsPrimitive(val.GetType) Then
-                str = val.ToString
-            ElseIf val.GetType.IsEnum Then
-                str = val.ToString
+        Public Function GetColorSetName() As String
+            If colors = CategoryPalettes.NA Then
+                Return colorSet
             Else
-                Throw New NotImplementedException(val.GetType.FullName)
+                Return colors.Description
+            End If
+        End Function
+
+        Public Function GetPadding() As Padding
+            Return New Padding(padding_left, padding_top, padding_right, padding_bottom)
+        End Function
+
+        Shared ReadOnly css_data As PropertyInfo() = GetType(PlotProperty) _
+            .GetProperties(DataFramework.PublicProperty) _
+            .Where(Function(p) p.GetIndexParameters.IsNullOrEmpty) _
+            .ToArray
+
+        Public Function GetJSON() As String
+            Dim json As New Dictionary(Of String, String)
+
+            For Each p As PropertyInfo In css_data
+                Dim val As Object = p.GetValue(Me)
+                Dim str As String
+
+                If val Is Nothing Then
+                    str = Nothing
+                ElseIf TypeOf val Is Color Then
+                    str = DirectCast(val, Color).ToHtmlColor
+                ElseIf TypeOf val Is Font Then
+                    str = New CSSFont(DirectCast(val, Font)).CSSValue
+                ElseIf DataFramework.IsPrimitive(val.GetType) Then
+                    str = val.ToString
+                ElseIf val.GetType.IsEnum Then
+                    str = val.ToString
+                Else
+                    Throw New NotImplementedException(val.GetType.FullName)
+                End If
+
+                Call json.Add(p.Name, str)
+            Next
+
+            Return json.GetJson(indent:=False, simpleDict:=True)
+        End Function
+
+        Public Shared Function ParseJSON(str As String) As PlotProperty
+            Dim json = str.LoadJSON(Of Dictionary(Of String, String))
+            Dim css As New PlotProperty
+            Dim val As Object
+
+            If json Is Nothing Then
+                Return Nothing
             End If
 
-            Call json.Add(p.Name, str)
-        Next
+            For Each p As PropertyInfo In css_data
+                str = json(p.Name)
+                val = Nothing
 
-        Return json.GetJson(indent:=False, simpleDict:=True)
-    End Function
+                If (Not str.StringEmpty) AndAlso Not str.TextEquals("null") Then
+                    Select Case p.PropertyType
+                        Case GetType(Color) : val = str.TranslateColor
+                        Case GetType(Font) : val = defaultCss.GetFont(CSSFont.TryParse(str))
+                        Case Else
+                            If DataFramework.IsPrimitive(p.PropertyType) Then
+                                val = any.CTypeDynamic(str, p.PropertyType)
+                            ElseIf p.PropertyType.IsEnum Then
+                                val = [Enum].Parse(p.PropertyType, str)
+                            Else
+                                Throw New NotImplementedException(p.PropertyType.FullName)
+                            End If
+                    End Select
+                End If
 
-    Public Shared Function ParseJSON(str As String) As PlotProperty
-        Dim json = str.LoadJSON(Of Dictionary(Of String, String))
-        Dim css As New PlotProperty
-        Dim val As Object
+                Call p.SetValue(css, val)
+            Next
 
-        If json Is Nothing Then
-            Return Nothing
-        End If
+            Return css
+        End Function
 
-        For Each p As PropertyInfo In css_data
-            str = json(p.Name)
-            val = Nothing
-
-            If (Not str.StringEmpty) AndAlso Not str.TextEquals("null") Then
-                Select Case p.PropertyType
-                    Case GetType(Color) : val = str.TranslateColor
-                    Case GetType(Font) : val = defaultCss.GetFont(CSSFont.TryParse(str))
-                    Case Else
-                        If DataFramework.IsPrimitive(p.PropertyType) Then
-                            val = any.CTypeDynamic(str, p.PropertyType)
-                        ElseIf p.PropertyType.IsEnum Then
-                            val = [Enum].Parse(p.PropertyType, str)
-                        Else
-                            Throw New NotImplementedException(p.PropertyType.FullName)
-                        End If
-                End Select
-            End If
-
-            Call p.SetValue(css, val)
-        Next
-
-        Return css
-    End Function
-
-End Class
+    End Class
+End Namespace
