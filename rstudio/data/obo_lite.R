9be156e13bc5ea646f73b5fe799d8d9d02ee61f3
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-02-16 15:50:27 +0800

    Update obo_lite.R

--- rstudio/data/obo_lite.R
+++ rstudio/data/obo_lite.R
@@ -9,6 +9,11 @@ setwd(@dir);
 obo = OBO::read.obo("./chebi.obo");
 obo 
 |> filter.is_obsolete() 
-|> filter_properties(["monoisotopicmass","inchikey","inchi","smiles"])
-|> write.obo( "./chebi_lite.obo",  excludes = ["synonym", "relationship","alt_id","xref","subset"])
+|> filter_properties([
+    "http://purl.obolibrary.org/obo/chebi/monoisotopicmass",
+    "http://purl.obolibrary.org/obo/chebi/inchikey",
+    "http://purl.obolibrary.org/obo/chebi/inchi",
+    "http://purl.obolibrary.org/obo/chebi/smiles"
+])
+|> write.obo( "./chebi_lite.obo",  excludes = ["synonym", "relationship","alt_id","xref","subset"], strip_namespace_prefix = "http://purl.obolibrary.org/obo/chebi/")
 ;
