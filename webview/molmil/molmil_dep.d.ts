454c79ce19b42d6fe5ae353dfcdfef8dc957f6af
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-03-05 10:40:20 +0800

    tsc

---
+++ webview/molmil/molmil_dep.d.ts
@@ -0,0 +1,10 @@
+/*!
+ * molmil_dep.js
+ *
+ * Molmil molecular web viewer: https://github.com/gjbekker/molmil
+ *
+ * By Gert-Jan Bekker
+ * License: LGPLv3
+ *   See https://github.com/gjbekker/molmil/blob/master/LICENCE.md
+ */
+declare var molmil_dep: any;
