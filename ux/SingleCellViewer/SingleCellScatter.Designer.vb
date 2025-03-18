77afd224c6021628567446e720b036463f909af2
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-03-18 10:16:22 +0800

    add color scalar ruler for the ion expression across multiple cells

--- ux/SingleCellViewer/SingleCellScatter.Designer.vb
+++ ux/SingleCellViewer/SingleCellScatter.Designer.vb
@@ -1,58 +1,60 @@
 ﻿#Region "Microsoft.VisualBasic::a732ffdefc267963cd61eba988597080, mzkit\ux\SingleCellViewer\SingleCellScatter.Designer.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 78
-    '    Code Lines: 56 (71.79%)
-    ' Comment Lines: 17 (21.79%)
-    '    - Xml Docs: 0.00%
-    ' 
-    '   Blank Lines: 5 (6.41%)
-    '     File Size: 3.31 KB
+' Summaries:
 
 
-    ' Class SingleCellScatter
-    ' 
-    '     Sub: Dispose, InitializeComponent
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 78
+'    Code Lines: 56 (71.79%)
+' Comment Lines: 17 (21.79%)
+'    - Xml Docs: 0.00%
+' 
+'   Blank Lines: 5 (6.41%)
+'     File Size: 3.31 KB
+
+
+' Class SingleCellScatter
+' 
+'     Sub: Dispose, InitializeComponent
+' 
+' /********************************************************************************/
 
 #End Region
+
+Imports ControlLibrary
 
 <Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class SingleCellScatter
@@ -81,6 +83,7 @@ Partial Class SingleCellScatter
         Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
         Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.ColorRuler = New ColorScaler
         Me.StatusStrip1.SuspendLayout()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
@@ -110,12 +113,18 @@ Partial Class SingleCellScatter
         Me.PictureBox1.Size = New System.Drawing.Size(1011, 644)
         Me.PictureBox1.TabIndex = 1
         Me.PictureBox1.TabStop = False
+
+
+        Me.ColorRuler.Dock = DockStyle.Right
+        Me.ColorRuler.Width = 50
+
         '
         'SingleCellScatter
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.ColorRuler)
         Me.Controls.Add(Me.StatusStrip1)
         Me.DoubleBuffered = True
         Me.Name = "SingleCellScatter"
@@ -131,4 +140,6 @@ Partial Class SingleCellScatter
     Friend WithEvents StatusStrip1 As StatusStrip
     Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
     Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents ColorRuler As ColorScaler
+
 End Class
