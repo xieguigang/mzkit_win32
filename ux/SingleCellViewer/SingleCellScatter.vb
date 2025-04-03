a159100d031d8ab44be8f1a8ef9a3946288279ee
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-04-04 00:17:04 +0800

    fix of the namespace reference error

--- ux/SingleCellViewer/SingleCellScatter.vb
+++ ux/SingleCellViewer/SingleCellScatter.vb
@@ -1,60 +1,60 @@
 ﻿#Region "Microsoft.VisualBasic::a88578857c484d6109a91490039784ad, mzkit\ux\SingleCellViewer\SingleCellScatter.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
-
-
-    ' Code Statistics:
-
-    '   Total Lines: 275
-    '    Code Lines: 211 (76.73%)
-    ' Comment Lines: 14 (5.09%)
-    '    - Xml Docs: 92.86%
-    ' 
-    '   Blank Lines: 50 (18.18%)
-    '     File Size: 10.11 KB
+' Summaries:
 
 
-    ' Class SingleCellScatter
-    ' 
-    '     Function: GetCanvas, GetCell, GetEmbedding, RenderScatter
-    ' 
-    '     Sub: FilterByCluster, FilterByUMAPSpace, LoadCells, LoadCellViews, LoadClusterData
-    '          LoadHeatmapData, PictureBox1_MouseClick, PictureBox1_MouseMove, PictureBox1_SizeChanged, RenderScatter
-    '          ResetDataView, SetRender, ShowMessage
-    ' 
-    ' /********************************************************************************/
+' Code Statistics:
+
+'   Total Lines: 275
+'    Code Lines: 211 (76.73%)
+' Comment Lines: 14 (5.09%)
+'    - Xml Docs: 92.86%
+' 
+'   Blank Lines: 50 (18.18%)
+'     File Size: 10.11 KB
+
+
+' Class SingleCellScatter
+' 
+'     Function: GetCanvas, GetCell, GetEmbedding, RenderScatter
+' 
+'     Sub: FilterByCluster, FilterByUMAPSpace, LoadCells, LoadCellViews, LoadClusterData
+'          LoadHeatmapData, PictureBox1_MouseClick, PictureBox1_MouseMove, PictureBox1_SizeChanged, RenderScatter
+'          ResetDataView, SetRender, ShowMessage
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -76,7 +76,7 @@ Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Math
 Imports Microsoft.VisualBasic.Math.LinearAlgebra
 Imports Microsoft.VisualBasic.MIME.Html.CSS
-Imports SingleExpression
+Imports SMRUCC.genomics.Analysis.SingleCell
 Imports std = System.Math
 
 Public Class SingleCellScatter
