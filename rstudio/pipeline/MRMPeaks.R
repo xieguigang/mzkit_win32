bc1f5ed730fd9a4a81cca7a312705f274fad504b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-06-23 09:45:10 +0800

    export ions peaks table

--- rstudio/pipeline/MRMPeaks.R
+++ rstudio/pipeline/MRMPeaks.R
@@ -5,6 +5,7 @@ imports "MRMLinear" from "mz_quantify";
 let MRMfiles = ?"--files" || stop("the input MRM mzXML files must be provided!");
 let ions = ?"--ions" || stop("the MRM ions for extract peaks data must be provided!");
 let outputdir = ?"--outdir" || dirname(MRMfiles);
+let args = MRM.arguments();
 
 MRMfiles = readLines(MRMfiles);
 ions = read.ion_pairs(ions) |> isomerism.ion_pairs(tolerance = "ppm:20");
@@ -14,4 +15,16 @@ print(MRMfiles);
 
 MRMfiles = lapply(MRMfiles, function(path) {
     let xic = extract.ions(path, ionpairs = ions, tolerance = "da:0.01");
+    let peaks = xic |> MRM.peakarea(args);
+
+    peaks= as.data.frame(peaks);
+    peaks[,"name"] = rownames(peaks);
+    peaks[,"source"] = basename(path);
+    peaks;
 });
+
+MRMfiles = bind_rows(MRMfiles);
+
+print(MRMfiles);
+
+write.csv(MRMfiles, file = file.path(outputdir, "MRMIons.csv"));
