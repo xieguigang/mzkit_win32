5a484751b203ecf91de8c591e7f407cbe5c6d973
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-06-23 09:05:32 +0800

    load data and extract peaks

--- rstudio/pipeline/MRMPeaks.R
+++ rstudio/pipeline/MRMPeaks.R
@@ -2,6 +2,7 @@ require(mzkit);
 
 let MRMfiles = ?"--files" || stop("the input MRM mzXML files must be provided!");
 let ions = ?"--ions" || stop("the MRM ions for extract peaks data must be provided!");
+let outputdir = ?"--outdir" || dirname(MRMfiles);
 
 MRMfiles = readLines(MRMfiles);
 
