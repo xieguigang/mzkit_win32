1d9ac17a88d1d66730786dbdb7a3658a9b45eacc
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-06-23 09:24:37 +0800

    Update MRMPeaks.R

--- rstudio/pipeline/MRMPeaks.R
+++ rstudio/pipeline/MRMPeaks.R
@@ -1,11 +1,17 @@
 require(mzkit);
 
+imports "MRMLinear" from "mz_quantify";
+
 let MRMfiles = ?"--files" || stop("the input MRM mzXML files must be provided!");
 let ions = ?"--ions" || stop("the MRM ions for extract peaks data must be provided!");
 let outputdir = ?"--outdir" || dirname(MRMfiles);
 
 MRMfiles = readLines(MRMfiles);
+ions = read.ion_pairs(ions) |> isomerism.ion_pairs(tolerance = "ppm:20");
 
 print("get MRM rawdata files:");
 print(MRMfiles);
 
+MRMfiles = lapply(MRMfiles, function(path) {
+    let xic = extract.ions(path, ionpairs = ions, tolerance = "da:0.01");
+});
