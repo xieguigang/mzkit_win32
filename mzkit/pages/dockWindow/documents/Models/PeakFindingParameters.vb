a7570cf6538ac65381a0698ea0553d8765bb6eaa
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-06-30 11:09:41 +0800

    config of the b-split pre-processing options

--- mzkit/pages/dockWindow/documents/Models/PeakFindingParameters.vb
+++ mzkit/pages/dockWindow/documents/Models/PeakFindingParameters.vb
@@ -91,6 +91,7 @@ Public Class PeakFindingParameters
 
     <Category("Peak Finding")>
     <DisplayName("baseline threshold")>
+    <Description("The quantile threshold for detects of the peak baseline(noise height).")>
     Public Property baseline_threshold As Double = 0.65
 
     <Category("Peak Finding")>
@@ -99,9 +100,22 @@ Public Class PeakFindingParameters
 
     <Category("Quantify Ion")>
     <DisplayName("error type")>
+    <Description("The mass error calculation method for matches Q1/Q3 ion")>
     Public Property toleranceMethod As MassToleranceType = MassToleranceType.Da
     <Category("Quantify Ion")>
+    <Description("The mass error for matches Q1/Q3 ion")>
     Public Property tolerance As Double = 0.3
+
+    <Category("Pre-Processing")>
+    <Description("Make pre-processing of the XIC chromatogram data?")>
+    Public Property preprocessing As Boolean
+    <Category("Pre-Processing")>
+    <DisplayName("B-spline degree")>
+    <Description("The degree for the b-spline interpolate processing of the XIC chromatogram data.")>
+    Public Property bspline_degree As Double = 2
+    <Category("Pre-Processing")>
+    <DisplayName("B-spline density")>
+    Public Property bspline_density As Integer = 10
 
     Sub New()
         Dim [default] = MRMArguments.GetDefaultArguments
@@ -114,6 +128,9 @@ Public Class PeakFindingParameters
         sn_threshold = [default].sn_threshold
         baseline_threshold = [default].baselineQuantile
         joint_peaks = [default].strict
+        preprocessing = False
+        bspline_degree = [default].bspline_degree
+        bspline_density = [default].bspline_density
     End Sub
 
     Public Function GetTolerance() As Tolerance
@@ -134,6 +151,9 @@ Public Class PeakFindingParameters
         args.sn_threshold = sn_threshold
         args.baselineQuantile = baseline_threshold
         args.joint_peaks = joint_peaks
+        args.bspline = preprocessing
+        args.bspline_degree = bspline_degree
+        args.bspline_density = bspline_density
 
         Return args
     End Function
