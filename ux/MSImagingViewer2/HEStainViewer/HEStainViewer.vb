ab631d16a4b373f4091db501ec4909abde5270f2
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-08-07 15:04:43 +0800

    render msi

--- ux/MSImagingViewer2/HEStainViewer/HEStainViewer.vb
+++ ux/MSImagingViewer2/HEStainViewer/HEStainViewer.vb
@@ -1,8 +1,12 @@
 ﻿Imports System.Drawing
 Imports System.IO
+Imports BioNovoGene.Analytical.MassSpectrometry
+Imports BioNovoGene.Analytical.MassSpectrometry.MsImaging
+Imports BioNovoGene.Analytical.MassSpectrometry.MsImaging.Blender
 Imports BioNovoGene.Analytical.MassSpectrometry.MsImaging.TissueMorphology
 Imports BioNovoGene.Analytical.MassSpectrometry.MsImaging.TissueMorphology.HEMap
 Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Imaging.Drawing2D.Colors
 Imports Microsoft.VisualBasic.Imaging.Drawing2D.HeatMap
 Imports Mzkit_win32.BasicMDIForm
 Imports Mzkit_win32.BasicMDIForm.CommonDialogs
@@ -11,7 +15,7 @@ Imports std = System.Math
 
 Public Class HEStainViewer
 
-    Dim MSIMatrix As PixelData()
+    Dim MSIMatrix As MsImaging.PixelData()
     Dim MSIDims As Size
 
     ''' <summary>
@@ -19,21 +23,33 @@ Public Class HEStainViewer
     ''' </summary>
     Dim HEImageSize As Size
     Dim HEBitmap As Bitmap
+    Dim MSIBitmap As Bitmap
 
     Private Sub HEStainViewer_Load(sender As Object, e As EventArgs) Handles Me.Load
         Me.DoubleBuffered = True
     End Sub
 
-    Public Function LoadRawData(MSI As IEnumerable(Of PixelData), msi_dims As Size, HEstain As Image) As SpatialTile
-        Me.MSIMatrix = MSI.ToArray
+    Public Function LoadRawData(Of PixelData As Pixel)(MSI As IEnumerable(Of PixelData), msi_dims As Size, HEstain As Image) As SpatialTile
+        Me.MSIMatrix = MSI.Select(Function(p) New MsImaging.PixelData(p.X, p.Y, p.Scale)).ToArray
         Me.MSIDims = msi_dims
         Me.HEImageSize = HEstain.Size
         Me.HEBitmap = New Bitmap(HEstain)
 
-
+        Call RenderMSI()
+        Call RefreshUI()
     End Function
 
+    Private Sub RenderMSI()
+        Dim heatmap As New Blender.PixelRender(False)
+        Dim args As New HeatMapParameters(ScalerPalette.turbo) With {.alpha = 80}
+        Dim msi As Image = heatmap.RenderPixels(MSIMatrix, MSIDims, New HeatMapBrushes(args)).AsGDIImage
 
+        MSIBitmap = msi
+    End Sub
+
+    Private Sub RefreshUI()
+
+    End Sub
 
     Public Sub SaveExport()
         Using file As New SaveFileDialog With {.Filter = "HEstain spatial register matrix(*.cdf)|*.cdf"}
