ae5ec1e0801f4f0ec9c0d3274734f35c5d84073d
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-09-10 13:47:34 +0800

    implements of save library table

--- mzkit/pages/dockWindow/documents/frmMoleculeLibrary.vb
+++ mzkit/pages/dockWindow/documents/frmMoleculeLibrary.vb
@@ -1,10 +1,13 @@
 ﻿Imports System.IO
 Imports System.Text
+Imports BioNovoGene.BioDeep.Chemoinformatics.Formula
 Imports Microsoft.VisualBasic.ComponentModel
+Imports Microsoft.VisualBasic.Data.Framework
 Imports Microsoft.VisualBasic.Net.Protocols.ContentTypes
 Imports Microsoft.VisualBasic.Text
 Imports Mzkit_win32.BasicMDIForm
 Imports Mzkit_win32.BasicMDIForm.CommonDialogs
+Imports any = Microsoft.VisualBasic.Scripting
 
 Public Class frmMoleculeLibrary : Implements IFileReference, ISaveHandle
 
@@ -20,6 +23,12 @@ Public Class frmMoleculeLibrary : Implements IFileReference, ISaveHandle
 
     Private Sub frmMoleculeLibrary_Load(sender As Object, e As EventArgs) Handles Me.Load
         Call ApplyVsTheme(ToolStrip1, AdvancedDataGridViewSearchToolBar1)
+
+        If ToolStripComboBox1.Items.Count > 0 Then
+            ToolStripComboBox1.SelectedIndex = 0
+        End If
+
+        ToolStripComboBox1_SelectedIndexChanged(Nothing, Nothing)
     End Sub
 
     ''' <summary>
@@ -49,6 +58,7 @@ Public Class frmMoleculeLibrary : Implements IFileReference, ISaveHandle
     Private Sub ToolStripButton2_Click(sender As Object, e As EventArgs) Handles ToolStripButton2.Click
         InputDialog.Input(Of InputImportsMetaboliteLibrary)(
             Sub(args)
+                Dim data As MetaInfoTable() = args.GetSource.ToArray
 
             End Sub)
     End Sub
@@ -87,12 +97,15 @@ Public Class frmMoleculeLibrary : Implements IFileReference, ISaveHandle
         End If
 
         Dim name As String = ToolStripComboBox1.SelectedItem.ToString
-        Dim libfile As String = New Configuration.Settings().MRMLibfile.ParentPath & $"/{name}.csv"
+        Dim libfile As String = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) & $"/mzkit/metabolites/{name}.csv"
 
         libcsvfile = libfile
 
         Call LoadLibrary()
     End Sub
+
+    Dim loader As GridLoaderHandler
+    Dim search As GridSearchHandler
 
     Sub LoadLibrary()
 
@@ -109,6 +122,45 @@ Public Class frmMoleculeLibrary : Implements IFileReference, ISaveHandle
     End Function
 
     Public Function Save(file As Stream, encoding As Encoding) As Boolean Implements ISaveHandle.Save
+        Dim rows As New List(Of MetaInfoTable)
+        Dim src As BindingSource = AdvancedDataGridView1.DataSource
+        Dim headers As New List(Of String)
 
+        For i As Integer = 0 To AdvancedDataGridView1.Columns.Count - 1
+            Call headers.Add(AdvancedDataGridView1.Columns(i).HeaderText)
+        Next
+
+        Dim id As Integer = headers.IndexOf(NameOf(MetaInfoTable.Id))
+        Dim name As Integer = headers.IndexOf(NameOf(MetaInfoTable.CommonName))
+        Dim formula As Integer = headers.IndexOf(NameOf(MetaInfoTable.Formula))
+        Dim cas As Integer = headers.IndexOf(NameOf(MetaInfoTable.cas))
+        Dim kegg As Integer = headers.IndexOf(NameOf(MetaInfoTable.kegg))
+        Dim hmdb As Integer = headers.IndexOf(NameOf(MetaInfoTable.hmdb))
+        Dim ds As System.Data.DataSet = src.DataSource
+        Dim table As DataTable = ds.Tables.Item(src.DataMember)
+
+        For j As Integer = 0 To table.Rows.Count - 1
+            Dim rowObj As DataRow = table.Rows(j)
+
+            Try
+                Dim vec = rowObj.ItemArray
+
+                If Not vec.IsNullOrEmpty Then
+                    Call rows.Add(New MetaInfoTable With {
+                        .cas = any.ToString(vec.ElementAtOrNull(cas)),
+                        .CommonName = any.ToString(vec.ElementAtOrNull(name)),
+                        .Formula = any.ToString(vec.ElementAtOrNull(formula)),
+                        .hmdb = any.ToString(vec.ElementAtOrNull(hmdb)),
+                        .Id = any.ToString(vec.ElementAtOrNull(id)),
+                        .kegg = any.ToString(vec.ElementAtOrNull(kegg)),
+                        .ExactMass = FormulaScanner.EvaluateExactMass(.Formula)
+                    })
+                End If
+            Catch ex As Exception
+                Call Workbench.Warning(ex.ToString)
+            End Try
+        Next
+
+        Return rows.SaveTo(file, New Arguments With {.encoding = encoding, .silent = True, .strict = False})
     End Function
 End Class
