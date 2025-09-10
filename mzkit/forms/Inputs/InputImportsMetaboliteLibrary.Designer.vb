09bf2dd7e93ab2140d9884d37089202691b6e93d
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-09-10 10:55:46 +0800

    configs of the library imports

--- mzkit/forms/Inputs/InputImportsMetaboliteLibrary.Designer.vb
+++ mzkit/forms/Inputs/InputImportsMetaboliteLibrary.Designer.vb
@@ -25,25 +25,42 @@ Partial Class InputImportsMetaboliteLibrary
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
-        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
         Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'GroupBox1
         '
+        Me.GroupBox1.Controls.Add(Me.Button3)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Label1)
         Me.GroupBox1.Controls.Add(Me.RichTextBox1)
         Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(410, 363)
+        Me.GroupBox1.Size = New System.Drawing.Size(463, 402)
         Me.GroupBox1.TabIndex = 0
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Imports Table Data"
         '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.RichTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.RichTextBox1.Location = New System.Drawing.Point(6, 20)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.ReadOnly = True
+        Me.RichTextBox1.Size = New System.Drawing.Size(451, 296)
+        Me.RichTextBox1.TabIndex = 0
+        Me.RichTextBox1.Text = ""
+        '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(315, 435)
+        Me.Button1.Location = New System.Drawing.Point(389, 429)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 1
@@ -52,32 +69,50 @@ Partial Class InputImportsMetaboliteLibrary
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(171, 422)
+        Me.Button2.Location = New System.Drawing.Point(290, 429)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(75, 23)
         Me.Button2.TabIndex = 2
         Me.Button2.Text = "Cancel"
         Me.Button2.UseVisualStyleBackColor = True
         '
-        'RichTextBox1
+        'Label1
         '
-        Me.RichTextBox1.Location = New System.Drawing.Point(49, 60)
-        Me.RichTextBox1.Name = "RichTextBox1"
-        Me.RichTextBox1.Size = New System.Drawing.Size(234, 191)
-        Me.RichTextBox1.TabIndex = 0
-        Me.RichTextBox1.Text = ""
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(15, 335)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(113, 12)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Select Table File:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(17, 360)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(372, 21)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(395, 358)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(57, 23)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "..."
+        Me.Button3.UseVisualStyleBackColor = True
         '
         'InputImportsMetaboliteLibrary
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(487, 537)
+        Me.ClientSize = New System.Drawing.Size(487, 465)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.GroupBox1)
         Me.Name = "InputImportsMetaboliteLibrary"
         Me.Text = "Imports Metabolite Library"
         Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
@@ -86,4 +121,7 @@ Partial Class InputImportsMetaboliteLibrary
     Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
     Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
 End Class
--- mzkit/forms/Inputs/InputImportsMetaboliteLibrary.vb
+++ mzkit/forms/Inputs/InputImportsMetaboliteLibrary.vb
@@ -1,6 +1,8 @@
-﻿Public Class InputImportsMetaboliteLibrary
+﻿Imports Microsoft.VisualBasic.Text
 
-    Shared ReadOnly rtf As XElement =
+Public Class InputImportsMetaboliteLibrary
+
+    Shared ReadOnly rtf As String =
         <rtf>
 {\rtf1\ansi\ansicpg936\deff0\nouicompat\deflang1033\deflangfe2052{\fonttbl{\f0\fnil\fcharset0 Calibri;}{\f1\fnil Calibri;}}
 {\*\generator Riched20 10.0.20348}\viewkind4\uc1 
@@ -23,6 +25,14 @@ This structured format ensures seamless integration into the database while acco
         </rtf>
 
     Private Sub InputImportsMetaboliteLibrary_Load(sender As Object, e As EventArgs) Handles Me.Load
-        RichTextBox1.Rtf = rtf.ToString
+        RichTextBox1.Rtf = rtf.Trim(" "c, ASCII.CR, ASCII.LF, ASCII.TAB)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.DialogResult = DialogResult.OK
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.DialogResult = DialogResult.Cancel
     End Sub
 End Class
