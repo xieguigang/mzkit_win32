f04bb144b0ef37e5bacbc1af7dbbc3f894240c04
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-10 10:07:13 +0800

    export and save the xlsx table file

--- ux/BasicMDIForm/plugin/modules/DataTableViewer.vb
+++ ux/BasicMDIForm/plugin/modules/DataTableViewer.vb
@@ -1,6 +1,4 @@
-﻿Imports System.IO
-Imports System.Runtime.CompilerServices
-Imports Microsoft.VisualBasic.Text
+﻿Imports System.Runtime.CompilerServices
 
 Public Module DataTableViewer
 
@@ -15,28 +13,6 @@ Public Module DataTableViewer
     <Extension>
     Public Sub OpenInTableViewer(matrix As DataGridView)
         Call openTable().LoadTable(AddressOf New GridLoader(matrix).LoadTable)
-    End Sub
-
-    ''' <summary>
-    ''' save data grid as excel table file
-    ''' </summary>
-    ''' <param name="table"></param>
-    ''' <param name="title">
-    ''' ``%s`` is the place holder for file name
-    ''' </param>
-    <Extension>
-    Public Sub SaveDataGrid(table As DataGridView, title$)
-        Using file As New SaveFileDialog With {
-            .Filter = "Excel Table(*.xls)|*.xls|Comma data sheet(*.csv)|*.csv",
-            .Title = $"Save Table File({title})"
-        }
-            If file.ShowDialog = DialogResult.OK Then
-                Using writeTsv As StreamWriter = file.FileName.OpenWriter(encoding:=Encodings.GB2312)
-                    Call table.WriteTableToFile(writeTsv, sep:=If(file.FileName.ExtensionSuffix("csv"), ","c, ASCII.TAB))
-                    Call MessageBox.Show(title.Replace("%s", file.FileName), "Export Table", MessageBoxButtons.OK, MessageBoxIcon.Information)
-                End Using
-            End If
-        End Using
     End Sub
 End Module
 
