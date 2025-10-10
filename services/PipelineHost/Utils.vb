c1ad557babe2d0bb39d14369332cfda431779dcc
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-10 11:43:37 +0800

    fix of the namespace reference error

--- services/PipelineHost/Utils.vb
+++ services/PipelineHost/Utils.vb
@@ -67,23 +67,6 @@ Imports SMRUCC.genomics.Analysis.Spatial.Imaging
 Public Module Utils
 
     <Extension>
-    Public Function GetMs1Points(raw As IEnumerable(Of ScanMS1)) As ms1_scan()
-        Return raw _
-            .Select(Function(m1)
-                        Return m1.mz _
-                            .Select(Function(mzi, i)
-                                        Return New ms1_scan With {
-                                            .mz = mzi,
-                                            .intensity = m1.into(i),
-                                            .scan_time = m1.rt
-                                        }
-                                    End Function)
-                    End Function) _
-            .IteratesALL _
-            .ToArray
-    End Function
-
-    <Extension>
     Private Iterator Function SpotConvertAsScans(spots As SpatialSpot(),
                                                  matrix As Matrix,
                                                  annotations As Dictionary(Of String, String)) As IEnumerable(Of ScanMS1)
