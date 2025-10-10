e8cc0d5ce6f8c5fdf08b24d4ce01890b1c90581d
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-10 09:39:12 +0800

    moves lcms data visual control to viewer project

--- ux/LCMSViewer/PeakFindingParameter.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class PeakFindingParameter
-
-    Public SN As Double = 1
-    Public baseline As Double = 0.65
-    Public peakwidth As Double() = {3, 15}
-    Public joint As Boolean = True
-    Public nticks As Integer = 6
-
-End Class
